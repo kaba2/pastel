a80cfe9df91befe2d73730030cdb9161c5ecc614
Kaba <Kaba@localhost>
2009-04-24 01:34:04 +0300

    Added missing table filter files.

---
+++ pastel/dsp/table_filter.cpp
@@ -0,0 +1,84 @@
+#include "pastel/dsp/table_filter.h"
+
+#include "pastel/sys/subview.h"
+#include "pastel/sys/view_tools.h"
+#include "pastel/sys/arrayview.h"
+
+#include "pastel/math/linear.h"
+
+namespace Pastel
+{
+
+	TableFilter::TableFilter(
+		const Array<1, real>& data,
+		real radius)
+		: Filter(radius, "table")
+		, data_()
+		, center_(0)
+		, scaling_(0)
+	{
+		const integer Padding = 1;
+		const integer width = data.width();
+
+		ENSURE(odd(width));
+
+		const integer paddedWidth = width + 2 * Padding;
+		
+		data_.setExtent(paddedWidth, 0);
+
+		copy(constArrayView(data),
+			subView(arrayView(data_), 
+			Rectangle<1>(Padding, paddedWidth - Padding)));
+
+		center_ = paddedWidth / 2;
+		scaling_ = (width / 2) / Filter::radius();
+	}
+
+	TableFilter::~TableFilter()
+	{
+	}
+
+	real TableFilter::evaluateInRange(real x) const
+	{
+		const real filterPos = 
+			center_ + x * scaling_;
+		
+		const integer filterIndex =
+			(integer)filterPos;
+
+		return linear(
+			data_(filterIndex),
+			data_(filterIndex + 1), 
+			filterPos - filterIndex);
+	}
+
+	void TableFilter::onSetRadius()
+	{
+		scaling_ = (data_.width() / 2) / radius();
+	}
+
+	PASTELDSP TableFilterPtr tableFilter(
+		const ConstFilterPtr& filter,
+		integer samplesPerRadius)
+	{
+		ENSURE1(samplesPerRadius >= 0, samplesPerRadius);
+
+		const real filterRadius = filter->radius();
+		const integer filterTableRadius = 
+			std::ceil(samplesPerRadius * filterRadius);
+		const integer filterTableSize = 
+			filterTableRadius * 2 + 1;
+		Array<1, real> filterTable(filterTableSize);
+		
+		for (integer i = 0;i < filterTableSize;++i)
+		{
+			const real filterPosition =
+				(2 * dequantizeUnsignedMatchEnds(i, filterTableSize) - 1) * filterRadius;
+			
+			filterTable(i) = filter->evaluateInRange(filterPosition);
+		}
+
+		return TableFilterPtr(new TableFilter(filterTable, filterRadius));
+	}
+
+}
---
+++ pastel/dsp/table_filter.h
@@ -0,0 +1,67 @@
+#ifndef PASTELDSP_TABLE_FILTER_H
+#define PASTELDSP_TABLE_FILTER_H
+
+#include "pastel/dsp/dsplibrary.h"
+#include "pastel/dsp/filter.h"
+
+#include "pastel/sys/array.h"
+
+namespace Pastel
+{
+
+	//! Piecewise-linear filter.
+	/*!
+	This class represents a piecewise-linear
+	filter whose knots are uniformly distributed
+	on the range [-radius, radius].
+	The first knot is located at x = -radius and
+	the last knot is located at x = radius.
+
+	The purpose of this class is to act
+	as an efficient approximation for the
+	other filters.
+	*/
+
+	class PASTELDSP TableFilter
+		: public Filter
+	{
+	public:
+		// Using default constructor.
+		// Using default copy constructor.
+		// Using default assignment.
+
+		//! Construct with the given data.
+		/*!
+		Preconditions:
+		radius > 0
+		odd(data.width())
+		*/
+		TableFilter(
+			const Array<1, real>& data,
+			real radius);
+
+		virtual ~TableFilter();
+
+		virtual real evaluateInRange(real position) const;
+
+	private:
+		// Prohibited
+		TableFilter(const TableFilter& that);
+		// Prohibited
+		TableFilter& operator=(const TableFilter& that);
+
+		virtual void onSetRadius();
+
+		Array<1, real> data_;
+		integer center_;
+		real scaling_;
+	};
+
+	typedef CountedPtr<TableFilter> TableFilterPtr;
+	typedef CountedPtr<const TableFilter> ConstTableFilterPtr;
+
+	PASTELDSP TableFilterPtr tableFilter(const ConstFilterPtr& filter, integer samplesPerRadius = 16);
+
+}
+
+#endif
