9e323cbbfadbaa7cb21b6b8d03f7555e227dcf5d
Kalle Rutanen <kaba@hilvi.org>
2009-07-15 03:20:05 +0300

    Added documentation.

---
+++ pastel/geometry/sphere_area.h
@@ -0,0 +1,31 @@
+// Description: Tools for computing sphere area
+// Documentation: area.txt
+
+#ifndef PASTEL_SPHERE_AREA_H
+#define PASTEL_SPHERE_AREA_H
+
+#include "pastel/geometry/geometrylibrary.h"
+#include "pastel/geometry/sphere.h"
+
+namespace Pastel
+{
+
+	// Note that we define area as the (n-1)-dimensional volume
+	// of the boundary.
+
+	PASTELGEOMETRY real areaUnitSphere(integer dimension);
+
+	template <int N, typename Real>
+	Real areaUnitSphere();
+
+	template <int N, typename Real>
+	Real areaUnitSphere(integer dimension);
+
+	template <int N, typename Real>
+	Real area(const Sphere<N, Real>& sphere);
+
+}
+
+#include "pastel/geometry/sphere_area.hpp"
+
+#endif
