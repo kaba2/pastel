cf65b42486490df0eede9a09a5337c8be978aeeb
kaba <kaba@localhost>
2009-07-15 03:20:05 +0300

    Added documentation.

---
+++ pastel/geometry/sphere_area.hpp
@@ -0,0 +1,35 @@
+#ifndef PASTEL_SPHERE_AREA_HPP
+#define PASTEL_SPHERE_AREA_HPP
+
+#include "pastel/geometry/sphere_area.h"
+
+#include "pastel/sys/math_functions.h"
+
+#include "pastel/sys/compiletime.h"
+
+namespace Pastel
+{
+
+	template <int N, typename Real>
+	Real areaUnitSphere()
+	{
+		BOOST_STATIC_ASSERT(N != Dynamic);
+		return Pastel::areaUnitSphere(N);
+	}
+
+	template <int N, typename Real>
+	Real areaUnitSphere(integer dimension)
+	{
+		return Pastel::areaUnitSphere(dimension);
+	}
+
+	template <int N, typename Real>
+	Real area(const Sphere<N, Real>& sphere)
+	{
+		return areaUnitSphere<N, Real>(sphere.dimension()) * 
+			std::pow(sphere.radius(), (Real)(sphere.dimension() - 1));
+	}
+
+}
+
+#endif
