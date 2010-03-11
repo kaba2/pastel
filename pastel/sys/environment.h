76d7f08b0255cecebbf2c4c0ddc2e75cf7f54b9e
kaba <kaba@localhost>
2010-03-11 21:13:51 +0200

    Added Google Analytics script. Forced PASTEL_ENABLE_PENSURES and PASTEL_DEBUG_MODE when _DEBUG is defined.

--- pastel/sys/environment.h
+++ pastel/sys/environment.h
@@ -4,9 +4,11 @@
 #define PASTEL_ENVIRONMENT_H
 
 // PASTEL_DEBUG_MODE == 0 => ASSERT:s will be removed.
+// If _DEBUG is defined, PASTEL_DEBUG_MODE is forced to 1.
 #define PASTEL_DEBUG_MODE 0
 
 // PASTEL_ENABLE_PENSURES == 0 => PENSURES will be removed.
+// If _DEBUG is defined, PASTEL_ENABLE_PENSURES is forced to 1.
 #define PASTEL_ENABLE_PENSURES 0
 
 // PASTEL_ENABLE_OMP != 0 => #include <omp.h> will be added where needed.
