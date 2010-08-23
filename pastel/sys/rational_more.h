d615ee7d3b8009171767cdc39b8e34ce2ea3e625
Kalle Rutanen <kaba@hilvi.org>
2010-08-23 04:46:49 +0300

    Tidied up Rational class.

--- pastel/sys/rational_more.h
+++ pastel/sys/rational_more.h
@@ -9,44 +9,57 @@
 namespace Pastel
 {
 
-	//! Returns (1/0).
+	//! Computes the multiplicative inverse of 'x'.
+	template <typename Integer>
+	Rational<Integer> inverse(const Rational<Integer>& x);
 
+	//! Computes the greatest integer less than or equal to x.
+	template <typename Integer>
+	Integer floor(const Rational<Integer>& x);
+
+	//! Computes the smallest integer greater than or equal to x.
+	template <typename Integer>
+	Integer ceil(const Rational<Integer>& x);
+
+	//! Returns (1/0).
 	template <typename Integer>
 	Rational<Integer> infinity(Rational<Integer>*);
 
 	//! Returns (0/0).
-
 	template <typename Integer>
 	Rational<Integer> nan(Rational<Integer>*);
 
-	//! Computes the fractional part of x e [0, 1[.
+	//! Returns that == 0.
+	template <typename Integer>
+	bool zero(const Rational<Integer>& that);
 
+	//! Returns that < 0.
+	template <typename Integer>
+	bool negative(
+		const Rational<Integer>& that);
+
+	//! Returns that > 0.
+	template <typename Integer>
+	bool positive(const Rational<Integer>& that);
+
+	template <typename Real, typename Integer>
+	Real toReal(const Rational<Integer>& that);
+
+	//! Computes the fractional part of x e [0, 1[.
 	/*!
 	This function is equivalent to remainder(x, 1).
 	This version offers an optimization opportunity
 	in this case.
 	*/
-
 	template <typename Integer>
 	Rational<Integer> fraction(
 		const Rational<Integer>& x);
 
 	//! Computes the remainder of (x / divider) e [0, divider[.
-
 	template <typename Integer>
 	Rational<Integer> remainder(
 		const Rational<Integer>& x,
 		const Rational<Integer>& divider);
-
-	//! Computes the greatest integer less than or equal to x.
-
-	template <typename Integer>
-	Integer floor(const Rational<Integer>& x);
-
-	//! Computes the smallest integer greater than or equal to x.
-
-	template <typename Integer>
-	Integer ceil(const Rational<Integer>& x);
 
 	// Optimization functions
 
@@ -59,22 +72,6 @@ namespace Pastel
 	Rational<Integer> divideByPowerOf2(
 		const Rational<Integer>& x,
 		integer power);
-
-	template <typename Integer>
-	Rational<Integer> inverse(const Rational<Integer>& x);
-
-	template <typename Integer>
-	bool negative(
-		const Rational<Integer>& that);
-
-	template <typename Integer>
-	bool positive(const Rational<Integer>& that);
-
-	template <typename Integer>
-	bool zero(const Rational<Integer>& that);
-
-	template <typename Real, typename Integer>
-	Real toReal(const Rational<Integer>& that);
 
 	template <typename Integer>
 	Rational<Integer> mabs(const Rational<Integer>& that);
