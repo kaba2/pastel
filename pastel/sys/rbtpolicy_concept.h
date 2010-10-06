13af16d97635033dbed51d5195796c8d206c2ae1
Kalle Rutanen <kaba@hilvi.org>
2010-10-06 21:20:09 +0300

    Updated documentation.

--- pastel/sys/rbtpolicy_concept.h
+++ pastel/sys/rbtpolicy_concept.h
@@ -18,12 +18,24 @@ namespace Pastel
 
 		//! Swaps two policies.
 		/*!
-		This operation should be O(1) to preserve
-		complexity guarantees.
+		Complexity: O(1).
 		*/
 		void swap(RbtPolicy_Concept& that);
 
 		//! Updates the hierarchical data in a node.
+		/*!
+		This function updates the hierarchical data for the
+		subtree pointed to by 'iter' under the assumption that 
+		the child subtrees are up to date. It is called by the
+		insert() and erase() functions of the RedBlackTree
+		whenever the structure of the subtree has changed.
+		Note that the complexities of the insert() and erase()
+		functions are multiplied by the complexity of this 
+		function. Thus, for example, to retain O(log n) 
+		complexity for those functions, this function must 
+		perform in O(1) (which is usually the case when the
+		hierarchical data is recursively defined).
+		*/
 		template <typename Iterator>
 		void updateHierarchicalData(const Iterator& iter);
 	};
