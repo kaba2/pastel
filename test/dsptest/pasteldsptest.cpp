97fd3ed9824de3b1365006fb84b6a2d034b59837
Kalle Rutanen <kaba@hilvi.org>
2010-11-24 01:57:04 +0200

    Moved tests from PastelGfxTest to PastelDspTest.

--- test/dsptest/pasteldsptest.cpp
+++ test/dsptest/pasteldsptest.cpp
@@ -2,6 +2,15 @@
 
 #include "pastel/sys/log_all.h"
 
+#include "pastel/gfx/pcx.h"
+#include "pastel/gfx/ewaimage_texture.h"
+#include "pastel/gfx/color_tools.h"
+
+#include "pastel/dsp/mipmap_tools.h"
+
+#include "pastel/sys/arrayextender.h"
+#include "pastel/sys/indexextender_all.h"
+
 using namespace Pastel;
 
 #include <string>
@@ -13,6 +22,26 @@ int main()
 {
 	log().addLogger(streamLogger(&std::cout));
 	log().addLogger(fileLogger("log.txt"));
+
+	Array<Color> textureImage;
+	loadPcx("lena.pcx", textureImage);
+
+	Array<real32> grayImage(textureImage.extent());
+	std::transform(
+		textureImage.begin(), textureImage.end(),
+		grayImage.begin(), luma);
+	
+	MipMap<Color> mipMap(constArrayView(textureImage));
+	EwaImage_Texture<Color> texture(mipMap, ArrayExtender<2, Color>(mirrorExtender()));
+	//NearestImage_Texture<Color> texture(textureImage, ArrayExtender<2, Color>(mirrorExtender()));
+	transform(mipMap, fitColor);
+
+	//grayImage.setExtent(128, 128);
+
+	dspStorage().set("lena_gray", &grayImage);
+	dspStorage().set("lena_image", &textureImage);
+	dspStorage().set("lena_mipmap", &mipMap);
+	dspStorage().set("lena_texture", &texture);
 
 	dspTestList().console();
 
