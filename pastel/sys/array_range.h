550a4cd1a1ae8172b62919df57c27f6d15ea90b7
Kalle Rutanen <kaba@hilvi.org>
2010-12-10 23:37:43 +0200

    Added new iterator ranges.

---
+++ pastel/sys/array_range.h
@@ -0,0 +1,112 @@
+#ifndef PASTEL_ARRAY_RANGE_H
+#define PASTEL_ARRAY_RANGE_H
+
+#include "pastel/sys/mytypes.h"
+#include "pastel/sys/ensure.h"
+
+namespace Pastel
+{
+
+	template <typename Type>
+	class Array_Range
+	{
+	public:
+		// Using default copy constructor.
+		// Using default assignment.
+		// Using default destructor.
+
+		typedef Type value_type;
+		typedef Type& reference;
+		static const bool RandomAccess = true;
+		static const bool Bidirectional = true;
+
+		Array_Range()
+			: begin_(0)
+			, end_(0)
+		{
+		}
+
+		Array_Range(Type* begin, Type* end)
+			: begin_(begin)
+			, end_(end)
+		{
+		}
+
+		void swap(Array_Range& that)
+		{
+			using std::swap;
+			swap(begin_, that.begin_);
+			swap(end_, that.end_);
+		}
+
+		bool empty() const
+		{
+			return begin_ >= end_;
+		}
+
+		void pop_front()
+		{
+			PENSURE(!empty());
+			++begin_;
+		}
+
+		reference front() const
+		{
+			PENSURE(!empty());
+			return *begin_;
+		}
+
+		integer size() const
+		{
+			return end_ - begin_;
+		}
+
+		void pop_back()
+		{
+			PENSURE(!empty());
+			--end_;
+		}
+
+		reference back() const
+		{
+			PENSURE(!empty());
+			return *(end_ - 1);
+		}
+
+		reference operator[](integer index) const
+		{
+			PENSURE_OP(index, >=, 0);
+			PENSURE_OP(index, <, size());
+
+			return *(begin_ + index);
+		}
+
+	private:
+		Type* begin_;
+		Type* end_;
+	};
+
+	template <typename Type, int N>
+	Array_Range<Type> arrayRange(
+		Type (&data)[N])
+	{
+		return Array_Range<Type>(data, data + N);
+	}
+
+	template <typename Type, int N>
+	Array_Range<Type> arrayRange(
+		Type* begin, Type* end)
+	{
+		return Array_Range<Type>(begin, end);
+	}
+
+	template <typename Type, int N>
+	Array_Range<Type> arrayRange(
+		Type* begin, integer size)
+	{
+		return Array_Range<Type>(begin, begin + size);
+	}
+
+}
+
+#endif
---
+++ pastel/sys/reverse_range.h
@@ -0,0 +1,102 @@
+#ifndef PASTEL_REVERSE_RANGE_H
+#define PASTEL_REVERSE_RANGE_H
+
+#include "pastel/sys/mytypes.h"
+#include "pastel/sys/ensure.h"
+
+namespace Pastel
+{
+
+	template <typename Range>
+	class Reverse_Range
+	{
+	public:
+		// Using default copy constructor.
+		// Using default assignment.
+		// Using default destructor.
+
+		typedef typename Range::value_type value_type;
+		typedef typename Range::reference reference;
+		static const bool RandomAccess = Range::RandomAccess;
+		static const bool Bidirectional = Range::Bidirectional;
+
+		PASTEL_STATIC_ASSERT(Bidirectional);
+
+		Reverse_Range()
+			: range_()
+		{
+			PENSURE_OP(range_.size(), >=, 0);
+		}
+
+		explicit Reverse_Range(
+			const Range& range)
+			: range_(range)
+		{
+			PENSURE_OP(range_.size(), >=, 0);
+		}
+
+		void swap(Reverse_Range& that)
+		{
+			range_.swap(that.range_);
+		}
+
+		bool empty() const
+		{
+			return range_.empty();
+		}
+
+		void pop_front()
+		{
+			range_.pop_back();
+		}
+
+		reference front() const
+		{
+			return range_.back();
+		}
+
+		integer size() const
+		{
+			return range_.size();
+		}
+
+		void pop_back()
+		{
+			range_.pop_front();
+		}
+
+		reference back() const
+		{
+			return range_.front();
+		}
+
+		const Range& reverse() const
+		{
+			return range_;
+		}
+
+		PASTEL_ENABLE_IF_C(RandomAccess, reference)
+			operator[](integer index)
+		{
+			return range_[size() - 1 - index];
+		}
+
+	private:
+		Range range_;
+	};
+
+	template <typename Range>
+	void reverseRange(const Range& range)
+	{
+		return Reverse_Range<Range>(range);
+	}
+
+	template <typename Range>
+	Range reverseRange(const Reverse_Range<Range>& range)
+	{
+		return range.reverse();
+	}
+
+}
+
+#endif
