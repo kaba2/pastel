63ec211838c1c735a6573ce9002f93871af93ae2
Kalle Rutanen <kaba@hilvi.org>
2011-01-27 03:38:26 +0200

    Added type checks to PastelMatlab argument functions.

--- pastel/matlab/matlab_argument.hpp
+++ pastel/matlab/matlab_argument.hpp
@@ -20,11 +20,15 @@ namespace Pastel
 
 	inline real asReal(const mxArray* input)
 	{
+		ENSURE(mxIsDouble(input));
+
 		return *mxGetPr(input);
 	}
 
 	inline std::string asString(const mxArray* input)
 	{
+		ENSURE(mxIsChar(input));
+
 		char* text = mxArrayToString(input);
 		std::string result(text);
 		mxFree(text);
@@ -36,6 +40,9 @@ namespace Pastel
 	inline boost::shared_ptr<Array<Type> > asArray(
 		const mxArray* that)
 	{
+		ENSURE(mxIsNumeric(that));
+		ENSURE_OP(typeToMatlabClassId<Type>(), ==, mxGetClassID(that));
+
 		// It is intentional to assign the width
 		// and height the wrong way. The reason
 		// is that Matlab uses column-major storage
@@ -56,6 +63,8 @@ namespace Pastel
 		const mxArray* cellArray,
 		ArrayPtr_Iterator output)
 	{
+		ENSURE(mxIsCell(cellArray));
+
 		const integer n = 
 			mxGetNumberOfElements(that);
 
@@ -82,12 +91,60 @@ namespace Pastel
 		const mxArray* input,
 		Real_Iterator output)
 	{
+		ENSURE(mxIsNumeric(input));
+
 		const integer n = 
 			mxGetNumberOfElements(input);
 
 		StdExt::copy_n(mxGetPr(input), n, output);
 		
 		return n;
+	}
+
+	namespace Matlab_Detail
+	{
+
+#define PASTEL_TYPE_TO_MATLAB_CLASSID(Type, Id) \
+		template <> \
+		class TypeToMatlabClassId<Type> \
+		{ \
+		public: \
+			mxClassID operator()() const \
+			{ \
+				return Id; \
+			} \
+		}
+
+		template <typename Type>
+		class TypeToMatlabClassId
+		{
+		public:
+			mxClassID operator()(const mxArray* that) const
+			{
+				return mxUNKNOWN_CLASS;
+			}
+		};
+
+        PASTEL_TYPE_TO_MATLAB_CLASSID(char, mxCHAR_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(void, mxVOID_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(real64, mxDOUBLE_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(real32, mxSINGLE_CLASS);
+        //PASTEL_TYPE_TO_MATLAB_CLASSID(int8, mxINT8_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(uint8, mxUINT8_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(int16, mxINT16_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(uint16, mxUINT16_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(int32, mxINT32_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(uint32, mxUINT32_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(int64, mxINT64_CLASS);
+        PASTEL_TYPE_TO_MATLAB_CLASSID(uint64, mxUINT64_CLASS);
+
+	}
+
+	template <typename Type>
+	mxClassID typeToMatlabClassId()
+	{
+		Matlab_Detail::TypeToMatlabClassId<Type> f;
+		return f();
 	}
 
 	namespace Matlab_Detail
