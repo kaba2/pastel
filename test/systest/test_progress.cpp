19ccb8e40217b2130d96fe6ed62f9febd7c78c40
Kaba <Kaba@localhost>
2011-02-07 01:43:04 +0200

    Added missing Progress test.

---
+++ test/systest/test_progress.cpp
@@ -0,0 +1,61 @@
+// Description: Testing for Progress class
+// DocumentationOf: progress.h
+
+#include "pastelsystest.h"
+
+#include "pastel/sys/progress.h"
+
+#include <iostream>
+#include <list>
+
+using namespace Pastel;
+
+namespace
+{
+
+	class Test
+		: public TestSuite
+	{
+	public:
+		Test()
+			: TestSuite(&sysTestReport())
+		{
+		}
+
+		virtual void run()
+		{
+			test();
+		}
+
+		void test()
+		{
+			int b = 0;
+			Progress progress(10, "progress");
+			for (integer i = 0;i < 10;++i)
+			{
+				int a = 0;
+				for (integer j = 0;j < (1 << 30);++j)
+				{
+					a += j * j;
+				}
+				b += a;
+				progress.report();
+			}
+			std::cout << b << std::endl;
+		}
+	};
+
+	void test()
+	{
+		Test theTest;
+		theTest.run();
+	}
+
+	void addTest()
+	{
+		sysTestList().add("Progress", test);
+	}
+
+	CallFunction run(addTest);
+
+}
