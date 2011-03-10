3e29bdf538e5fd7c704ff4632a1cbaa2e682ee3c
Kaba <Kaba@localhost>
2011-03-10 22:13:33 +0200

    Updated build system for Mac consumption.

--- pastel/geometrymatlab/pastelgeometrymatlab.cpp
+++ pastel/geometrymatlab/pastelgeometrymatlab.cpp
@@ -2,16 +2,14 @@
 
 #define FORCE_LINKING(name) \
 	void force_linking_##name(); \
-	force_linking_##name()
-
-namespace
-{
-
-	void forceLinking()
-	{
-		FORCE_LINKING(point_pattern_match);
-		FORCE_LINKING(point_pattern_match_gmo);
-		FORCE_LINKING(pointkdtree);
+	namespace \
+	{ \
+		void call_force_linking_##name() \
+		{ \
+			force_linking_##name(); \
+		} \
 	}
 
-}
+FORCE_LINKING(point_pattern_match);
+FORCE_LINKING(point_pattern_match_gmo);
+FORCE_LINKING(pointkdtree);
