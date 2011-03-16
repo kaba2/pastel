ca2ecad6f82d4cfc80789c72c6fd97bc437202c8
Kalle Rutanen <kaba@hilvi.org>
2011-03-16 17:16:48 +0200

    Updated build system.

--- pastel/geometrymatlab/pastelgeometrymatlab.cpp
+++ pastel/geometrymatlab/pastelgeometrymatlab.cpp
@@ -2,12 +2,9 @@
 
 #define FORCE_LINKING(name) \
 	void force_linking_##name(); \
-	namespace \
+	void call_force_linking_##name() \
 	{ \
-		void call_force_linking_##name() \
-		{ \
-			force_linking_##name(); \
-		} \
+		force_linking_##name(); \
 	}
 
 FORCE_LINKING(point_pattern_match);
