95f499b4cb27b71ebf69d1ed9b16d18c396ed44b
Kaba <Kaba@localhost>
2011-07-15 07:15:52 +0300

    Updated PastelGeometryMatlab Matlab build files. Improved the gmo ppm test.

--- pastel/geometrymatlab/test_point_pattern_match_gmo.m
+++ pastel/geometrymatlab/test_point_pattern_match_gmo.m
@@ -15,18 +15,19 @@ matchingDistance = 0.01;
 
 % The amplitude of the gaussian noise to apply to the model
 % points in the scene.
-%noiseAmount = 0.02;
-noiseAmount = 0.06;
+noiseAmount = 0.02;
+%noiseAmount = 0;
 
+%missingPointsPercentage = 0.7;
 missingPointsPercentage = 0.7;
 
 % Generate a random set of model points.
 
 d = 2;
-m = 200;
+m = 500;
 M = randn(d, m);
 
-missingPoints = floor(m * missingPointsPercentage);
+missingPoints = min(floor(m * missingPointsPercentage), m);
 
 % Generate a random translation.
 translation = rand(d, 1) * 5;
@@ -40,8 +41,12 @@ S = M + kron(ones(1, m), translation) + noise;
 % algorithm surely has no way to take advantage of 
 % pairing.
 n = size(S, d);
-S = S(:, randperm(n));
-M = M(:, randperm(m));
+
+scenePermutation = randperm(n);
+modelPermutation = randperm(m);
+
+S = S(:, scenePermutation);
+M = M(:, modelPermutation);
 
 % Get rid of some of the scene points.
 S = S(:, 1 : n - missingPoints);
@@ -54,21 +59,32 @@ tic
     minMatchRatio, matchingDistance);
 timeSpent = toc;
 
-%pairSet
+foundPairSet = zeros(1, m);
+foundPairSet(modelPermutation(pairSet(2, :))) = ...
+    scenePermutation(pairSet(1, :));
+
+correctPairSet = 1 : m;
+correctPairs = sum(foundPairSet == correctPairSet);
+%A = [correctPairSet; foundPairSet]'
 
 % Output the results.
+
+matchSize = size(pairSet, 2);
+
 if success
-    fprintf('Found a match!\n');
+    fprintf('Found a match with %d points!\n', matchSize);
+    fprintf('Out of these points, %d match with the correct pair.\n', ...
+        correctPairs);
+    fprintf('Here are the returned parameters, ');
+    fprintf('correct parameters in parentheses:\n');
+    fprintf('xDelta:   %f (%f)\n', ...
+        nTranslation(1), translation(1));
+    fprintf('yDelta:   %f (%f)\n', ...
+        nTranslation(2), translation(2));
 else
     fprintf('No match was found.\n');
 end
-fprintf('The matching took %f s.\n', timeSpent);
-fprintf('Here are the returned parameters, ');
-fprintf('correct parameters in parentheses:\n');
-fprintf('xDelta:   %f (%f)\n', ...
-    nTranslation(1), translation(1));
-fprintf('yDelta:   %f (%f)\n\n', ...
-    nTranslation(2), translation(2));
+fprintf('The matching took %f s.\n\n', timeSpent);
 
 % Visualize the point-sets.
 nM = M + kron(ones(1, m), nTranslation);
@@ -76,6 +92,24 @@ nM = M + kron(ones(1, m), nTranslation);
 figure;
 hold on;    
 title('Transformed model points (blue) vs scene points (red)');
-scatter(S(1, :), S(2, :), 100, 'rx');
+scatter(S(1, :), S(2, :), 100, 'r.');
 scatter(nM(1, :), nM(2, :), 100, 'b');
+
+[ignore, modelPermutationInv] = sort(modelPermutation);
+[ignore, scenePermutationInv] = sort(scenePermutation);
+
+for i = 1 : matchSize
+    sceneIndex = pairSet(1, i);
+    modelIndex = pairSet(2, i);
+    correctModelIndex = modelPermutationInv(...
+        scenePermutation(sceneIndex));
+
+    line([S(1, sceneIndex); nM(1, modelIndex)], ...
+        [S(2, sceneIndex); nM(2, modelIndex)], 'Color', 'black');
+    if modelIndex ~= correctModelIndex
+        line([S(1, sceneIndex); nM(1, correctModelIndex)], ...
+            [S(2, sceneIndex); nM(2, correctModelIndex)], 'Color', 'red');
+    end
+end
+    
 hold off;
