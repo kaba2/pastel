9e460ee3c6a2719587dae0fd1ba2382ea74c1184
kaba <kaba@localhost>
2012-05-03 19:01:55 +0300

    Improved the Tree class.

--- pastel/sys/tree.h
+++ pastel/sys/tree.h
@@ -11,6 +11,7 @@
 namespace Pastel
 {
 
+	//! A generic binary tree
 	template <typename Type, typename Derived = EmptyClass>
 	class Tree
 	{
@@ -33,22 +34,38 @@ namespace Pastel
 		typedef Tree_Private::Node<Data> Node;
 
 	public:
+		//! Construct an empty tree.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		strong
+		*/
 		Tree()
 			: sentinel_(0)
 			, leftMost_(0)
 			, root_(0)
 			, size_(0)
 		{
-			construct();
+			initialize();
 		}
 
+		//! Copy-constructs from another tree.
+		/*!
+		Time complexity:
+		O(that.size())
+
+		Exception safety:
+		strong
+		*/
 		Tree(const Tree& that)
 			: sentinel_(0)
 			, leftMost_(0)
 			, root_(0)
 			, size_(0)
 		{
-			construct();
+			initialize();
 
 			try
 			{
@@ -62,46 +79,152 @@ namespace Pastel
 			catch(...)
 			{
 				clear();
+				
+				// The destructor will not be run,
+				// so remember to deinitialize.
+				deinitialize();
+
 				throw;
 			}
 		}
 
+		//! Move-constructs from another tree.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		strong
+		*/
 		Tree(Tree&& that)
 			: sentinel_(0)
 			, leftMost_(0)
 			, root_(0)
 			, size_(0)
 		{
-			construct();
+			initialize();
 
+			// The sentinel node of 'that' is not
+			// preserved. This is ok, since
+			// 'that' will not be used anymore.
 			swap(that);
 		}
 
+		//! Destructs the tree.
+		/*!
+		Time complexity:
+		O(size())
+
+		Exception safety: 
+		nothrow
+		*/
 		~Tree()
 		{
 			clear();
-
-			// The sentinel node should not be
-			// destructed with deallocate(). It
-			// has no user data.
-			delete sentinel_;
-			sentinel_ = 0;
+			deinitialize();
 		}
 
+		//! Copy-assigns from another tree.
+		/*!
+		Time complexity:
+		O(size() + that.size())
+
+		Exception safety: 
+		strong
+		*/
 		Tree& operator=(const Tree& that)
 		{
-			Tree copy(that);
-			swap(copy);
+			// Note that we want to preserve the sentinel node.
+			// This is why we do this rather than copy and swap.
+
+			// Detach the current tree.
+			Node* oldRoot = root_;
+			Node* oldLeftMost = leftMost_;
+			Node* oldRightMost = sentinel_->parent;
+			integer oldSize = size_;
+
+			root_ = sentinel_;
+			leftMost_ = sentinel_;
+			sentinel_->parent = sentinel_;
+			size_ = 0;
+
+			try
+			{
+				// Create a copy of 'that'.
+				Iterator copyRoot = 
+					insert(end(), Tree::Right, that);
+			}
+			catch(...)
+			{
+				// Attach the old tree back.
+				root_ = oldRoot;
+				leftMost_ = oldLeftMost;
+				sentinel_->parent = oldRightMost;
+				size_ = oldSize;
+
+				throw;
+			}
+
+			Node* newRoot = root_;
+			Node* newLeftMost = leftMost_;
+			Node* newRightMost = sentinel_->parent;
+			integer newSize = size_;
+
+			// Switch the old tree back.
+			root_ = oldRoot;
+			leftMost_ = oldLeftMost;
+			sentinel_->parent = oldRightMost;
+			size_ = oldSize;
+
+			// Destruct the old tree.
+			clear();
+
+			// Attach the copy of 'that' back.
+			root_ = newRoot;
+			leftMost_ = newLeftMost;
+			sentinel_->parent = newRightMost;
+			size_ = newSize;
+			
 			return *this;
 		}
 
+		//! Move-assigns from another tree.
+		/*!
+		Complexity:
+		O(size() + that.size())
+
+		Exception safety: 
+		strong
+		*/
 		Tree& operator=(Tree&& that)
 		{
-			Tree copy(that);
-			swap(copy);
+			// Note that we want to preserve the sentinel nodes.
+
+			clear();
+
+			root_ = that.root_;
+			leftMost_ = that.leftMost_;
+			sentinel_->parent = that.sentinel_->parent;
+			size_ = that.size_;
+
+			that.root_ = that.sentinel_;
+			that.leftMost_ = that.sentinel_;
+			that.sentinel_->parent = that.sentinel_;
+			that.size_ = 0;
+
+			updateSentinel(root_);
+			
 			return *this;
 		}
 
+		//! Swaps two trees.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		void swap(Tree& that)
 		{
 			using std::swap;
@@ -112,16 +235,40 @@ namespace Pastel
 			swap(size_, that.size_);
 		}
 
+		//! Returns if the tree is empty.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		bool empty() const
 		{
 			return size_ == 0;
 		}
 
+		//! Returns the number of nodes in the tree.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		integer size() const
 		{
 			return size_;
 		}
 		
+		//! Destructs all nodes in the tree.
+		/*!
+		Time complexity:
+		O(size())
+
+		Exception safety: 
+		nothrow
+		*/
 		void clear()
 		{
 			if (!empty())
@@ -134,66 +281,176 @@ namespace Pastel
 			size_ = 0;
 		}
 
+		//! Returns the root node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		Iterator root()
 		{
 			return Iterator(root_);
 		}
 
+		//! Returns the root node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstIterator root() const
 		{
 			return ConstIterator(root_);
 		}
 
+		//! Returns the root node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstIterator croot() const
 		{
 			return ConstIterator(root_);
 		}
 
+		//! Returns the first node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		Iterator begin()
 		{
 			return Iterator(leftMost_);
 		}
 
+		//! Returns the first node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstIterator begin() const
 		{
 			return ConstIterator(leftMost_);
 		}
 
+		//! Returns the first node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstIterator cbegin() const
 		{
 			return ConstIterator(leftMost_);
 		}
 
+		//! Returns the one-past-last node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		Iterator end()
 		{
 			return Iterator(sentinel_);
 		}
 
+		//! Returns the one-past-last node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstIterator end() const
 		{
 			return ConstIterator(sentinel_);
 		}
 
+		//! Returns the one-past-last node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstIterator cend() const
 		{
 			return ConstIterator(sentinel_);
 		}
 
+		//! Returns a range to the element sequence.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		Range range()
 		{
-			return Range(begin(), end());
+			return Range(begin(), end(), size_);
 		}
 
+		//! Returns a range to the element sequence.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstRange range() const
 		{
-			return ConstRange(begin(), end());
+			return ConstRange(begin(), end(), size_);
 		}
 
+		//! Returns a range to the element sequence.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety: 
+		nothrow
+		*/
 		ConstRange crange() const
 		{
-			return ConstRange(cbegin(), cend());
+			return ConstRange(cbegin(), cend(), size_);
 		}
 
+		//! Inserts an element into the root.
+		/*!
+		Preconditions:
+		empty()
+
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		strong
+
+		returns:
+		An iterator to the root node.
+		*/
 		Iterator insertRoot(const Data& data = Data())
 		{
 			ENSURE(empty());
@@ -201,7 +458,6 @@ namespace Pastel
 			// The children of the sentinel node always
 			// refer to itself. Thus we do not set the
 			// child pointer here.
-				
 			Node* node = allocate(data);
 
 			// The rightmost node is stored at the
@@ -218,6 +474,17 @@ namespace Pastel
 			return Iterator(node);
 		}
 
+		//! Inserts a new node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		strong
+
+		returns:
+		An iterator to the inserted node.
+		*/
 		Iterator insert(
 			const ConstIterator& here, 
 			integer childIndex,
@@ -275,19 +542,42 @@ namespace Pastel
 			return Iterator(node);
 		}
 
-		void insert(
+		//! Copies a tree under a node.
+		/*!
+		Time complexity:
+		O(that.size())
+
+		Exception safety:
+		strong
+
+		returns:
+		An iterator to the root of the copied tree.
+		*/
+		Iterator insert(
 			const ConstIterator& there, 
 			integer childIndex,
 			const Tree& that)
 		{
 			PENSURE_OP(childIndex, >=, 0);
 			PENSURE_OP(childIndex, <, 2);
-			
+
+			if (that.empty())
+			{
+				return cast(there);
+			}			
+
 			integer rollback = 0;
-			ConstIterator copyRoot;
+			Iterator copyRoot;
 			try
 			{
-				copyRoot = insert(there, childIndex, *that.root());
+				if (there.sentinel())
+				{
+					copyRoot = insertRoot(*that.root());
+				}
+				else
+				{
+					copyRoot = insert(there, childIndex, *that.root());
+				}
 				++rollback;
 
 				copyConstruct(that.root(), copyRoot, copyRoot);
@@ -304,9 +594,22 @@ namespace Pastel
 				
 				throw;
 			}
+
+			return copyRoot;
 		}
 
-		void insert(
+		//! Moves a tree under a node.
+		/*!
+		Time complexity:
+		O(that.size())
+
+		Exception safety:
+		nothrow
+
+		returns:
+		An iterator to the root of the moved tree.
+		*/
+		Iterator insert(
 			const ConstIterator& there, 
 			integer childIndex,
 			Tree&& that)
@@ -318,51 +621,79 @@ namespace Pastel
 			{
 				return;
 			}
-			
-			integer rollback = 0;
-			ConstIterator copyRoot;
-			try
-			{
 
-				Node* thatNode = (Node*)that.root().node_;
+			Node* thatRoot = that.root_;
+
+			if (there.sentinel())
+			{
+				const bool emptyWhenInsertingToSentinel =
+					empty();
+				ENSURE(emptyWhenInsertingToSentinel);
+
+				root_ = thatRoot;
+				leftMost_ = that.leftMost_;
+				rightMost_ = that.rightMost_;
+				size_ = that.size_;
+			}
+			else
+			{			
 				Node* thereNode = (Node*)there.node_;
 
-				if (thereNode != sentinel_)
-				{
-					thereNode->childSet[childIndex] = thatNode;
-				}
-				thatNode->parent = parent;
-
-				if (thereNode == leftMost_)
-				{
-					leftMost_ = that.leftMost_;
-				}
+				Node*& childRef = thereNode->childSet[childIndex];
 				
-				Node*& rightMost = sentinel_->parent;
-				if (thereNode == rightMost)
+				// Check that the child does not already exist.
+				const bool childAlreadyExists = 
+					(childRef != sentinel_);
+				ENSURE(!childAlreadyExists);
+
+				// Link 'that' tree to 'there' node.
+				childRef = thatRoot;
+				thatNode->parent = thereNode;
+
+				// Possibly update the leftmost node.
+				if (childIndex == Left)
 				{
-					rightMost = that.rightMost_;
+					if (thereNode == leftMost_)
+					{
+						leftMost_ = that.leftMost_;
+					}
 				}
 
+				// Possibly update the rightmost node.
+				if (childIndex == Right)		
+				{				
+					Node*& rightMost = sentinel_->parent;
+					if (thereNode == rightMost)
+					{
+						rightMost = that.rightMost_;
+					}
+				}
+
+				// Update the size.
 				size_ += that.size_;
+			}
 
-				that.leftMost_ = that.sentinel_;
-				that.root_ = that.sentinel_;
-				that.size_ = 0;
-			}
-			catch(...)
-			{
-				switch(rollback)
-				{
-				case 1:
-					erase(copyRoot);
-					break;
-				};
-				
-				throw;
-			}
+			// Change the sentinel nodes to refer to 
+			// this tree.
+			updateSentinel(thatRoot);
+
+			// Get rid of the tree in 'that'.
+			that.leftMost_ = that.sentinel_;
+			that.root_ = that.sentinel_;
+			that.sentinel_->parent_ = that.sentinel_;
+			that.size_ = 0;
+
+			return Iterator(thatRoot);
 		}
 
+		//! Removes a node.
+		/*!
+		Time complexity:
+		O(size(node))
+
+		Exception safety:
+		nothrow
+		*/
 		void erase(const ConstIterator& that)
 		{
 			PENSURE(!that.sentinel());
@@ -401,6 +732,18 @@ namespace Pastel
 			return tree;
 		}
 
+		//! Rotates a node.
+		/*!
+		Preconditions:
+		!that.child(R).child(L).sentinel()
+		where L = direction, and R = !L.
+
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		nothrow
+		*/
 		void rotate(
 			const ConstIterator& that,
 			integer direction)
@@ -437,8 +780,29 @@ namespace Pastel
 			}
 		}
 
+		//! Converts a const-iterator to an iterator.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		nothrow
+		*/
+		Iterator cast(const ConstIterator& that)
+		{
+			return Iterator((Node*)that.node_);
+		}
+
 	private:
-		void construct()
+		//! Initializations common to all constructors.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		strong
+		*/
+		void initialize()
 		{
 			// The sentinel node should not be
 			// constructed with allocate(). It
@@ -449,6 +813,33 @@ namespace Pastel
 			root_ = sentinel_;
 		}
 
+		//! Deinitializations common to all 'destructors'.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		nothrow
+		*/
+		void deinitialize()
+		{
+			// The sentinel node should not be
+			// destructed with deallocate(). It
+			// has no user data.
+			delete sentinel_;
+			sentinel_ = 0;
+			leftMost_ = 0;
+			size_ = 0;
+		}
+
+		//! Computes the size of a subtree recursively.
+		/*!
+		Time complexity:
+		O(size(node))
+
+		Exception safety:
+		nothrow
+		*/
 		integer size(Node* node)
 		{
 			ASSERT(node);
@@ -472,6 +863,14 @@ namespace Pastel
 			return result;
 		}
 
+		//! Removes a subtree recursively.
+		/*!
+		Time complexity:
+		O(size(node))
+
+		Exception safety:
+		nothrow
+		*/
 		void erase(Node* node)
 		{
 			ASSERT(node);
@@ -515,13 +914,40 @@ namespace Pastel
 			deallocate(node);
 		}
 
+		//! Allocates a node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		strong
+		*/
 		Node* allocate(const Data& data)
 		{
-			Node* node = new Node(sentinel_);
+			integer rollBack = 0;
+			Node* node = 0;
 
-			if (node->data())
+			try
 			{
-				new (node->data()) Data(data);
+				node = new Node(sentinel_);
+				++rollBack;
+			
+				if (node->data())
+				{
+					new (node->data()) Data(data);
+					++rollBack;
+				}
+			}
+			catch(...)
+			{
+				switch(rollBack)
+				{
+				case 1:
+					delete node;
+					break;
+				};
+
+				throw;
 			}
 
 			++size_;
@@ -529,6 +955,14 @@ namespace Pastel
 			return node;
 		}
 
+		//! Deallocates a node.
+		/*!
+		Time complexity:
+		O(1)
+
+		Exception safety:
+		nothrow
+		*/
 		void deallocate(Node* node)
 		{
 			ASSERT(node);
@@ -565,6 +999,39 @@ namespace Pastel
 						insert(to, i, *newFrom);
 
 					copyConstruct(newFrom, newTo, forbidden);
+				}
+			}
+		}
+
+		//! Turn all sentinel references to this tree.
+		/*!
+		Time complexity:
+		O(size(node))
+
+		Exception safety:
+		nothrow
+		*/
+		void updateSentinel(Node* node)
+		{
+			ASSERT(!node->sentinel());
+
+			for (integer i = 0;i < 2;++i)
+			{
+				Node*& childRef = node->childSet[i];
+
+				if (childRef->sentinel())
+				{
+					// This is a sentinel reference,
+					// but not necessary to this tree.
+					
+					// Turn the reference to this tree.
+					childRef = sentinel_;
+				}
+				else
+				{
+					// This is a normal node reference.
+					// Recurse.
+					updateSentinel(childRef);
 				}
 			}
 		}
