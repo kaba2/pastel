df4231802c12650fe2433ba20e0420818c112017
kaba <kaba@localhost>
2012-07-03 18:43:06 +0300

    Factored types from mytypes.h under the namespace Types for easy inclusion to other libraries (and also injected that namespace to Pastel). Changed transitive closure to report only the domain elements.

--- pastel/sys/mytypes.h
+++ pastel/sys/mytypes.h
@@ -65,147 +65,154 @@
 namespace Pastel
 {
 
-	// Shortened native types
-
-	typedef signed char        schar;
-
-	typedef long long          longlong;
-
-	typedef unsigned char      uchar;
-	typedef unsigned short     ushort;
-	typedef unsigned int       uint;
-	typedef unsigned long      ulong;
-	typedef unsigned long long ulonglong;
-
-	// Absolute size types
-
-	// PORTABILITY: the size of the native
-	// types is not defined in standard C++.
-	// We check these assumptions at compile-time
-	// in pastelsys.cpp.
-
-	typedef char               int8;
-	typedef short              int16;
-	typedef int                int32;
-	typedef long long          int64;
-
-	typedef unsigned char      uint8;
-	typedef unsigned short     uint16;
-	typedef unsigned int       uint32;
-	typedef unsigned long long uint64;
-
-	typedef float              real32;
-	typedef double             real64;
-	typedef float              real32_ieee;
-	typedef double             real64_ieee;
-
-	namespace MyTypes_
+	namespace Types
 	{
 
-		template <int N>
-		struct IntegerOfSize {};
+		// Shortened native types
 
-		template <>
-		struct IntegerOfSize<2>
+		typedef signed char        schar;
+
+		typedef long long          longlong;
+
+		typedef unsigned char      uchar;
+		typedef unsigned short     ushort;
+		typedef unsigned int       uint;
+		typedef unsigned long      ulong;
+		typedef unsigned long long ulonglong;
+
+		// Absolute size types
+
+		// PORTABILITY: the size of the native
+		// types is not defined in standard C++.
+		// We check these assumptions at compile-time
+		// in pastelsys.cpp.
+
+		typedef char               int8;
+		typedef short              int16;
+		typedef int                int32;
+		typedef long long          int64;
+
+		typedef unsigned char      uint8;
+		typedef unsigned short     uint16;
+		typedef unsigned int       uint32;
+		typedef unsigned long long uint64;
+
+		typedef float              real32;
+		typedef double             real64;
+		typedef float              real32_ieee;
+		typedef double             real64_ieee;
+
+		namespace MyTypes_
 		{
-			typedef int16 type;
+
+			template <int N>
+			struct IntegerOfSize {};
+
+			template <>
+			struct IntegerOfSize<2>
+			{
+				typedef int16 type;
+			};
+
+			template <>
+			struct IntegerOfSize<4>
+			{
+				typedef int32 type;
+			};
+
+			template <>
+			struct IntegerOfSize<8>
+			{
+				typedef int64 type;
+			};
+
+		}
+
+		//! Integer capable of holding a pointer.
+		/*!
+		Preconditions:
+		sizeof(void*) == sizeof(pointer_integer)
+		'pointer_integer' is a native signed integer type.
+		*/
+		typedef MyTypes_::IntegerOfSize<sizeof(void*)>::type 
+			pointer_integer;
+
+		//! Abstract native integer type
+		/*!
+		Preconditions:
+		sizeof(integer) >= sizeof(int32)
+		sizeof(integer) <= sizeof(int64)
+		'integer' is a native signed integer type.
+	
+		If PASTEL_LARGE_INTEGER is defined, then additionally:
+		sizeof(integer) >= min(sizeof(void*), sizeof(int64)) 
+		*/
+	#	ifdef PASTEL_LARGE_INTEGER
+			typedef std::conditional<
+				sizeof(void*) <= sizeof(int64),
+				pointer_integer, int64>::type integer;
+	#	else
+			typedef int32 integer;
+	#	endif
+	
+		//! Abstract native real type
+		/*!
+		Preconditions:
+		'real' is a native floating point type.
+		*/
+		typedef real64 real;
+
+		//! Integer for holding hash integers.
+		/*!
+		An integer of this type is used to hold hash
+		values.
+		*/
+		typedef uint32 hash_integer;
+
+		//! Removes brackets around a type.
+		/*!
+		To transmit a type containing commas through a macro,
+		it needs to be enclosed in brackets. However, then it
+		is possible that the type does not make sense anymore.
+		Therefore, PASTEL_REMOVE_BRACKETS macro turns this
+		bracketed type into a function type 'void (T)'. From
+		this form we can again regain the type inside the
+		brackets using RemoveBrackets.
+		*/
+		template <typename T>
+		class RemoveBrackets
+		{
+		};
+
+		template <typename T>
+		class RemoveBrackets<void (T)>
+		{
+		public:
+			typedef T Type;
 		};
 
 		template <>
-		struct IntegerOfSize<4>
+		class RemoveBrackets<void ()>
 		{
-			typedef int32 type;
+		public:
+			typedef void Type;
 		};
 
-		template <>
-		struct IntegerOfSize<8>
-		{
-			typedef int64 type;
-		};
+		// An integer literal for documenting concepts.
+		static const integer UserDefinedInteger = 0;
+		// A boolean literal for documenting concepts.
+		static const bool UserDefinedBoolean = true;
+		// A type for documenting concepts.
+		class PASTELSYS UserDefinedType {};
+
+		static const integer Dynamic = -1;
+		//enum {Dynamic = -1};
+
+		class PASTELSYS EmptyClass {};
 
 	}
 
-	//! Integer capable of holding a pointer.
-	/*!
-	Preconditions:
-	sizeof(void*) == sizeof(pointer_integer)
-	'pointer_integer' is a native signed integer type.
-	*/
-	typedef MyTypes_::IntegerOfSize<sizeof(void*)>::type 
-		pointer_integer;
-
-	//! Abstract native integer type
-	/*!
-	Preconditions:
-	sizeof(integer) >= sizeof(int32)
-	sizeof(integer) <= sizeof(int64)
-	'integer' is a native signed integer type.
-	
-	If PASTEL_LARGE_INTEGER is defined, then additionally:
-	sizeof(integer) >= min(sizeof(void*), sizeof(int64)) 
-	*/
-#	ifdef PASTEL_LARGE_INTEGER
-		typedef std::conditional<
-			sizeof(void*) <= sizeof(int64),
-			pointer_integer, int64>::type integer;
-#	else
-		typedef int32 integer;
-#	endif
-	
-	//! Abstract native real type
-	/*!
-	Preconditions:
-	'real' is a native floating point type.
-	*/
-	typedef real64 real;
-
-	//! Integer for holding hash integers.
-	/*!
-	An integer of this type is used to hold hash
-	values.
-	*/
-	typedef uint32 hash_integer;
-
-	//! Removes brackets around a type.
-	/*!
-	To transmit a type containing commas through a macro,
-	it needs to be enclosed in brackets. However, then it
-	is possible that the type does not make sense anymore.
-	Therefore, PASTEL_REMOVE_BRACKETS macro turns this
-	bracketed type into a function type 'void (T)'. From
-	this form we can again regain the type inside the
-	brackets using RemoveBrackets.
-	*/
-	template <typename T>
-	class RemoveBrackets
-	{
-	};
-
-	template <typename T>
-	class RemoveBrackets<void (T)>
-	{
-	public:
-		typedef T Type;
-	};
-
-	template <>
-	class RemoveBrackets<void ()>
-	{
-	public:
-		typedef void Type;
-	};
-
-	// An integer literal for documenting concepts.
-	static const integer UserDefinedInteger = 0;
-	// A boolean literal for documenting concepts.
-	static const bool UserDefinedBoolean = true;
-	// A type for documenting concepts.
-	class PASTELSYS UserDefinedType {};
-
-	static const integer Dynamic = -1;
-	//enum {Dynamic = -1};
-
-	class PASTELSYS EmptyClass {};
+	using namespace Pastel::Types;
 
 	//! A no-op function taking a variable.
 	/*!
