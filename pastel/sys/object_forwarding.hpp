16926ebfebaa4a45abff16e023443413b660f3c9
Kalle Rutanen <kaba@hilvi.org>
2012-07-27 21:31:00 +0300

    Simplified object forwarding.

--- pastel/sys/object_forwarding.hpp
+++ pastel/sys/object_forwarding.hpp
@@ -4,6 +4,9 @@
 #include "pastel/sys/object_forwarding.h"
 #include "pastel/sys/mytypes.h"
 
+#include <utility>
+#include <type_traits>
+
 namespace Pastel
 {
 
@@ -14,75 +17,14 @@ namespace Pastel
 		// Only non-class types are allowed to be forwarded.
 		PASTEL_STATIC_ASSERT(!std::is_class<Type>::value);
 
-		// FIX: Once variadic templates become available in
-		// Visual Studio, replace these constructors with
-		// the variadic forwarder.
-
 		Class()
 			: member_()
 		{
 		}
 
-		template <typename P1>
-		Class(P1&& p1)
-			: member_(
-			std::forward<P1>(p1))
-		{
-		}
-
-		template <
-			typename P1, typename P2>
-			Class(P1&& p1, P2&& p2)
-			: member_(
-			std::forward<P1>(p1), std::forward<P2>(p2))
-		{
-		}
-
-		template <
-			typename P1, typename P2, 
-			typename P3>
-			Class(P1&& p1, P2&& p2, P3&& p3)
-			: member_(
-			std::forward<P1>(p1), std::forward<P2>(p2),
-			std::forward<P3>(p3))
-		{
-		}
-
-		template <
-			typename P1, typename P2, 
-			typename P3, typename P4>
-			Class(P1&& p1, P2&& p2, P3&& p3, P4&& p4)
-			: member_(
-			std::forward<P1>(p1), std::forward<P1>(p2),
-			std::forward<P3>(p3), std::forward<P4>(p4))
-		{
-		}
-
-		template <
-			typename P1, typename P2, 
-			typename P3, typename P4,
-			typename P5>
-			Class(
-			P1&& p1, P2&& p2, P3&& p3, P4&& p4, 
-			P5&& p5)
-			: member_(
-			std::forward<P1>(p1), std::forward<P2>(p2),
-			std::forward<P3>(p3), std::forward<P4>(p4),
-			std::forward<P5>(p5))
-		{
-		}
-
-		template <
-			typename P1, typename P2, 
-			typename P3, typename P4,
-			typename P5, typename P6>
-			Class(
-			P1&& p1, P2&& p2, P3&& p3, P4&& p4, 
-			P5&& p5, P6&& p6)
-			: member_(
-			std::forward<P1>(p1), std::forward<P2>(p2),
-			std::forward<P3>(p3), std::forward<P4>(p4),
-			std::forward<P5>(p5), std::forward<P6>(p6))
+		template <typename That>
+		Class(That&& that)
+			: member_(std::forward<That>(that))
 		{
 		}
 
