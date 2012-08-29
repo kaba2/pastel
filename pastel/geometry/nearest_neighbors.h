1ba17c2328b5af9cffd2792c23bc02fac0eeb733
Kalle Rutanen <kaba@hilvi.org>
2012-08-29 17:55:49 +0300

    Added missing files.

---
+++ pastel/geometry/nearest_neighbors.h
@@ -0,0 +1,15 @@
+// Description: An aggregate file nearest neighbors searching.
+
+#ifndef PASTELGEOMETRY_NEAREST_NEIGHBORS_H
+#define PASTELGEOMETRY_NEAREST_NEIGHBORS_H
+
+#include "pastel/geometry/acceptpoint.h"
+#include "pastel/geometry/count_nearest_pointkdtree.h"
+#include "pastel/geometry/count_all_neighbors.h"
+#include "pastel/geometry/search_nearest_pointkdtree.h"
+#include "pastel/geometry/search_nearest_one_pointkdtree.h"
+#include "pastel/geometry/search_all_neighbors.h"
+#include "pastel/geometry/bestfirst_searchalgorithm_pointkdtree.h"
+#include "pastel/geometry/depthfirst_searchalgorithm_pointkdtree.h"
+
+#endif
