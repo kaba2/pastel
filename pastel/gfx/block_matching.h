7e94c4aae0337194e3a2741b3cf4b37f057145e5
Kalle Rutanen <kaba@hilvi.org>
2012-08-30 18:40:20 +0300

    Removed files.

--- pastel/gfx/block_matching.h
+++
@@ -1,8 +0,0 @@
-// Description: An aggregate file for block matching.
-
-#ifndef PASTELGFX_BLOCK_MATCHING_H
-#define PASTELGFX_BLOCK_MATCHING_H
-
-#include "pastel/gfx/block_matching_bruteforce.h"
-
-#endif
