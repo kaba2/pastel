55129f9abd2231c98ad1ca5bed281d93fd81cc63
kaba <kaba@localhost>
2012-09-21 22:23:37 +0300

    Removed some files.

--- pastel/geometry/intersection_graph.h
+++
@@ -1,8 +0,0 @@
-// Description: An aggregate file for intersection graph algorithms.
-
-#ifndef PASTELGEOMETRY_INTERSECTION_GRAPH_H
-#define PASTELGEOMETRY_INTERSECTION_GRAPH_H
-
-#include "pastel/geometry/maximum_clique_alignedbox.h"
-
-#endif
