81b05d564e336062290be2d3e7db6fa5108a1470
kaba <kaba@localhost>
2012-10-26 14:57:13 +0300

    Updated documentation. Added minIterations to ICP.

--- pastel/math/random_spd_matrix.hpp
+++ pastel/math/random_spd_matrix.hpp
@@ -62,110 +62,6 @@ namespace Pastel
 		const PASTEL_NO_DEDUCTION(Real)& condition,
 		Matrix<Real>& result)
 	{
-		/*
-		Problem:
-		Generate a random symmetric positive 
-		definite (n x n)-matrix	S with 
-		determinant d and condition number c.
-
-		Solution:
-		Let R be an orthogonal (n x n)-matrix.
-		Let D be a positive (n x n)-diagonal matrix.
-		Let S = RDR^T.
-
-		Then 
-		
-		S is symmetric:
-
-		S^T = (RDR^T)^T = R D^T R^T = RDR^T = S
-
-		S is positive definite:
-
-		(for all x in R^n:)
-		x^T S x > 0
-		<=>
-		x^T R D R^T x > 0
-		<=> (R is invertible)
-		x^T D x > 0
-		<=> (D is positive diagonal)
-		true
-
-		det(S) 
-		= det(RDR^T) 
-		= det(R) det(D) det(R^T)
-		= det(D)
-
-		The generation of a random rotation matrix R
-		is described in the function 'setRandomRotation'.
-		Now we just need to generate D such that det(S) = d,
-		and cond(S) = c. Note that all symmetric positive
-		definite matrices can be brought to the form
-		of S by eigendecomposition.
-
-		Let D = diag(e^(-a), e^(-b), e^(-b), ..., e^(-b)).
-		and assume a >= b.
-		
-		Then
-
-		cond(S) 
-		= cond(D) 
-		= e^(-b) / e^(-a)
-		= e^a / e^b
-		= e^(a - b)
-
-		det(S)
-		= det(D)
-		= e^(-a) e^(-b(n - 1))
-		
-		Thus we get the pair of equations:
-		(1) cond(S) = e^(a - b) = c
-		(2) det(S) = e^(-a) e^(-b(n - 1)) = d
-		
-		(1)
-		<=>
-		a - b = ln(c)
-		<=>
-		b = a - ln(c)
-
-		(2)
-		<=>
-		-a - b(n - 1) = ln(d)
-		<=>
-		a + b(n - 1) = -ln(d)
-		(3)
-		
-		(1) -> (3)
-		<=>
-		a + (a - ln(c))(n - 1) = -ln(d)
-		<=>
-		an - (n - 1) ln(c) = -ln(d)
-		<=>
-		a = [(n - 1) ln(c) - ln(d)] / n
-		<=>
-		(4)
-
-		(4) -> (1)
-		<=>
-		b = [(n - 1) ln(c) - ln(d) - n ln(c)] / n
-		= [-ln(c) - ln(d)] / n
-		= -[ln(c) + ln(d)] / n
-		
-		Thus the solution is:
-		a = [(n - 1) ln(c) - ln(d)] / n
-		b = -[ln(c) + ln(d)] / n
-
-		We assumed 
-		a >= b
-		<=>
-		a - b >= 0
-		<=>
-		ln(c) >= 0
-		<=>
-		c >= 1
-
-		This is the only restriction.
-		*/
-
 		ENSURE_OP(result.width(), ==, result.height());
 		ENSURE_OP(condition, >=, 1);
 		ENSURE_OP(determinant, >, 0);
@@ -182,12 +78,10 @@ namespace Pastel
 			a - std::log(condition);
 
 		// Generate a random rotation matrix.
-
 		setRandomRotation(result);
 
 		// Multiply the columns of the rotation matrix
-		// with square root of the diagonal element of D.
-
+		// with square roots of the diagonal elements of D.
 		for (integer i = 0;i < n;++i)
 		{
 			result(i, 0) *= std::exp(-a / 2);
