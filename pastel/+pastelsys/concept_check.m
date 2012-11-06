68ea711a99e38460703df3755cbe2477327483f5
kaba <kaba@localhost>
2012-11-07 01:56:28 +0200

    Updated documentation.

--- pastel/+pastelsys/concept_check.m
+++ pastel/+pastelsys/concept_check.m
@@ -1,3 +1,17 @@
+% CONCEPT_CHECK
+% Concept checking for objects
+%
+% concept_check(object, 'concept', ...)
+%
+% For each object, CONCEPT_CHECK calls a function x_concept(object), 
+% where x is the name of the concept. Such a concept check function
+% returns a cell-array of strings which detail the ways in which the
+% object fails to conform to the concept. If the cell-array is non-empty,
+% the information is printed on the screen and then an error is emitted;
+% otherwise no actions are taken.
+
+% Description: Concept checking for objects
+
 function concept_check(varargin)
 
 eval(import_pastel);
@@ -18,7 +32,10 @@ for i = 1 : n
             'Now it is a ', class(conceptName), '.']);
     end
     
+    % Check the concept.
     messageSet = eval([conceptName, '_concept(object);']);
+    
+    % Print the possible error messages.
     m = numel(messageSet);
     if m > 0
         objectName = inputname(2 * i - 1);
