72a716e09f7fd136e4d7581991b0f71bc789465f
Kalle Rutanen <kaba@hilvi.org>
2012-11-23 17:12:24 +0200

    Generalized ls_affine.m for more transformations.

--- pastel/+pastelgeometry/ls_affine.m
+++ pastel/+pastelgeometry/ls_affine.m
@@ -1,7 +1,7 @@
 % LS_AFFINE
 % Optimal affine transformation between point-sets.
 %
-% [A, t, s] = ls_affine(fromSet, toSet, 'key', value, ...)
+% [Q, S, t] = ls_affine(fromSet, toSet, 'key', value, ...)
 %
 % where
 %
@@ -12,25 +12,27 @@
 % the coordinates of a d-dimensional point. If W is not specified,
 % it must hold that m = n.
 %
-% A is a (d x d) real matrix, containing the matrix-part of the
-% optimal transformation.
+% Q is a (d x d) real orthogonal matrix, representing a rotation or
+% a reflection.
 %
-% T is a (d x 1) real vector containing the optimal translation.
+% S is a (d x d) real symmetric matrix, representing a scaling. Note 
+% that by the eigenvalue decomposition, a symmetric matrix is the 
+% composition of a rotation, axis-aligned scaling, and a reverse rotation. 
 %
-% S is a non-negative real number containing the optimal scaling.
+% T is a (d x 1) real matrix, representing a translation.
 %
 % Optional input arguments in 'key'-value pairs:
 %
 % MATRIX ('matrix') is a string which specifies constraints 
-% for the matrix A. Must be one of
-%     free: A free (default)
-%     orthogonal: A^T A = I
-%     identity: A = I
+% for the matrix Q. Must be one of
+%     free: Q^T Q = I (default)
+%     identity: Q = I
 %
 % SCALING ('scaling') is a string which specifies constraints
-% for the scaling s. Must be one of
-%     free: S >= 0 (default)
-%     rigid: S = 1
+% for the scaling S. Must be one of
+%     free: S^T = S (default)
+%     conformal: S = sI
+%     rigid: S = I
 %
 % TRANSLATION ('translation') is a string which specifies constraints 
 % for the translation t. Must be one of
@@ -38,28 +40,28 @@
 %     identity: T = 0
 %
 % ORIENTATION ('orientation') is an integer which specifies constraints
-% for the determinant of A. Must be one of
-%    -1: det(A) < 0,
-%     0: det(A) free (default), or
-%     1: det(A) > 0.
+% for the determinant of QS. Must be one of
+%    -1: det(QS) < 0,
+%     0: det(QS) free (default), or
+%     1: det(QS) > 0.
 %
 % W ('W') is a (m x n) non-negative real matrix, which contains the 
 % weights for the least-squares error metric.
 %
-% The A, T, and S are chosen such that they minimize the error metric
+% The Q, S, and T are chosen such that they minimize the error metric
 %
-%      sum_{i = 1}^m sum_{j = 1}^n w_{ij} ||(s * A * p_i + t) - r_j||^2
+%      sum_{i = 1}^m sum_{j = 1}^n w_{ij} ||(QS p_i + t) - r_j||^2
 %
 % subject to the given constraints.
 %
 % If W is not given, then it is required that m = n, and it is assumed
 % that W = eye(m, n). Then the error metric simplifies to
 %
-%      sum_{i = 1}^m ||(s * A * p_i + t) - r_i||^2.
+%      sum_{i = 1}^m ||(QS p_i + t) - r_i||^2.
 
 % Description: Optimal affine transformation between point-sets.
 
-function [A, t, s] = ls_affine(fromSet, toSet, varargin)
+function [Q, S, t] = ls_affine(fromSet, toSet, varargin)
 
 eval(import_pastel);
 
@@ -95,15 +97,15 @@ if iscell(W) && size(fromSet, 2) ~= size(toSet, 2)
 end
 
 % Check matrix.
-matrixSet = {'free', 'orthogonal', 'identity'};
+matrixSet = {'free', 'identity'};
 if ~ismember(matrix, matrixSet)
-    error('MATRIX must be either free, orthogonal, or identity.');
+    error('MATRIX must be either free or identity.');
 end
 
 % Check scaling.
-scalingSet = {'free', 'rigid'};
+scalingSet = {'free', 'conformal', 'rigid'};
 if ~ismember(scaling, scalingSet)
-    error('SCALING must be either free or rigid.');
+    error('SCALING must be either free, conformal, or rigid.');
 end
 
 % Check translation.
@@ -116,6 +118,14 @@ end
 orientationSet = [-1, 0, 1];
 if ~any(orientation == orientationSet)
     error('ORIENTATION must be one of -1, 0, or 1.')
+end
+
+if strcmp(scaling, 'free') && orientation ~= 0
+    warning('Orientation can not be forced when SCALING is free.');
+end
+
+if strcmp(matrix, 'identity') && orientation ~= 0
+    warning('Orientation can not be forced when MATRIX is identity.');
 end
 
 d = size(fromSet, 1);
@@ -137,7 +147,8 @@ if strcmp(translation, 'free')
     end
     
     % Form the centered point-sets. The optimal transformation
-    % will map fromCentroid to toCentroid.
+    % will map fromCentroid to toCentroid. After this the problem
+    % has been reduced from affine to linear.
     P = P - fromCentroid * ones(1, m);
     R = R - toCentroid * ones(1, n);
 end
@@ -146,10 +157,20 @@ if ~iscell(W)
     c = sum(W, 2);
 end
 
-if strcmp(matrix, 'identity')
-    % Use the identity matrix for A.
-    A = eye(d, d);
-elseif strcmp(matrix, 'free')
+% Defaults for Q and S.
+Q = eye(d, d);
+S = eye(d, d);
+
+if strcmp(scaling, 'free') && strcmp(matrix, 'identity')
+    % f(x) = Sx
+    
+    % Find the optimal scaling.
+    S = lyap(P * P', -(R * P' + P * R'));
+end
+
+if strcmp(scaling, 'free') && strcmp(matrix, 'free')
+    % f(x) = Ax
+    
     % Compute the optimal linear transformation.
     if ~iscell(W)
         A = (R * W' * P') / (P * diag(c) * P');
@@ -157,35 +178,42 @@ elseif strcmp(matrix, 'free')
         A = (R * P') / (P * P');
     end
     
-    if orientation ~= 0
-        warning('Orientation can not be forced when the MATRIX is free.')
-    end
-else
+    % Compute Q and S from A such that
+    % A = QS and S is symmetric positive semi-definite.
+    [U, D, V] = svd(A);
+    Q = U * V';
+    S = V * D * V';
+end
+
+if ~strcmp(scaling, 'free') && strcmp(matrix, 'free')
+    % f(x) = sQx
+    
     % Compute the optimal orthogonal transformation.
     if ~iscell(W)
         [U, D, V] = svd(R * W' * P');
     else
         [U, D, V] = svd(R * P');
     end
-    A = U * V';
+    Q = U * V';
 
     if orientation ~= 0
         % If orientation is to be forced at det(A) = g, where g = +- 1,
         % then A is given by:
         %
-        %    A = UDV^T, where
+        %    Q = UDV^T, where
         %    D = [1, ..., 1, g det(UV^T)].
 
         % Compute the optimal oriented orthogonal Q.
         o = double(orientation);
-        if sign(det(A)) ~= sign(o)
-            A = U * diag([ones(1, d - 1), -1]) * V';
+        if sign(det(Q)) ~= sign(o)
+            Q = U * diag([ones(1, d - 1), -1]) * V';
         end
     end
 end
 
-s = 1;
-if strcmp(scaling, 'free') && ~strcmp(matrix, 'free')
+if strcmp(scaling, 'conformal')
+    % f(x) = sQx
+    
     % Compute tr(P^T diag(c) P).
     if ~iscell(W)
         tracePtp = sum(sum((P * diag(c)).^2));
@@ -194,12 +222,13 @@ if strcmp(scaling, 'free') && ~strcmp(matrix, 'free')
     end
     
     % Compute the optimal scaling parameter.
-    s = trace(D) / tracePtp;
+    s = trace(Q' * R * P') / tracePtp;
+    S = s * eye(d, d);
 end
 
 t = zeros(d, 1);
 if strcmp(translation, 'free')
     % Compute the optimal translation.
-    t = toCentroid - s * A * fromCentroid;
+    t = toCentroid - Q * S * fromCentroid;
 end
 
