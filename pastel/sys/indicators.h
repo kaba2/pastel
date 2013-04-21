dcb9c5b736d353bd4eacf8bd459da4358b11a2ec
kaba <kaba@localhost>
2013-04-21 17:01:21 +0300

    Added empty and single indicators.

--- pastel/sys/indicators.h
+++ pastel/sys/indicators.h
@@ -1,11 +1,12 @@
 // Description: An aggregate file for indicators.
 
-#ifndef PASTELSYS_INDICATOR_H
-#define PASTELSYS_INDICATOR_H
+#ifndef PASTELSYS_INDICATORS_H
+#define PASTELSYS_INDICATORS_H
 
 #include "pastel/sys/all_indicator.h"
 #include "pastel/sys/allexcept_indicator.h"
-#include "pastel/sys/objectdont_indicator.h"
 #include "pastel/sys/complement_indicator.h"
+#include "pastel/sys/empty_indicator.h"
+#include "pastel/sys/single_indicator.h"
 
 #endif
