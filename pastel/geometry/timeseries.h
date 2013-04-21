7e2c90a30f735114942239cac382e4580b57e02c
Kalle Rutanen <kaba@hilvi.org>
2013-04-22 00:50:19 +0300

    Started to work on TimeSeries.

---
+++ pastel/geometry/timeseries.h
@@ -0,0 +1,41 @@
+// Description: Time series
+
+#ifndef PASTELGEOMETRY_TIMESERIES_H
+#define PASTELGEOMETRY_TIMESERIES_H
+
+#include "pastel/geometry/timeseries_concepts.h"
+#include "pastel/geometry/pointkdtree.h"
+
+#include <memory>
+
+namespace Pastel
+{
+
+	template <typename Args>
+	class TimeSeries
+	{
+	public:
+		typedef typename Args::PointRep PointRep;
+		typedef typename PointRep::Real Real;
+		typedef typename PointRep::Point Point;
+		enum {N = PointRep::N};
+
+	private:
+		class Node;
+		typedef std::unique_ptr<Node> NodePtr;
+
+		class Node
+		{
+		public:
+			integer tSplit_;
+			NodePtr left_;
+			NodePtr right_;
+		};
+		
+		NodePtr root_;
+		integer tBegin_;
+	};
+
+}
+
+#endif
