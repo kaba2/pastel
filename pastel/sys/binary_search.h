c2a76f3b774a4df1278d5b2d571d8d25df807216
Kalle Rutanen <kaba@hilvi.org>
2013-09-24 02:45:21 +0300

    Refactored binary searching.

--- pastel/sys/binary_search.h
+++ pastel/sys/binary_search.h
@@ -1,5 +1,4 @@
-// Description: Binary search over an integer range
-// Documentation: algorithms.txt
+// Description: Binary search
 
 #ifndef PASTELSYS_BINARY_SEARCH_H
 #define PASTELSYS_BINARY_SEARCH_H
@@ -31,40 +30,6 @@ namespace Pastel
 	*/
 	template <typename Integer, typename Integer_Indicator>
 	Integer binarySearch(
-		Integer minLevel, Integer maxLevel,
-		Integer_Indicator indicator);
-
-	//! Exponential binary-search over an integer range.
-	/*!
-	This algorithm improves the time complexity over the normal
-	binary search. The price to pay for this is that the algorithm
-	has a hidden constant of four compared to the normal binary
-	search. For example, it takes four times as much time to
-	search for the maximum element. On the other hand, it takes
-	only a constant time to find the minimum element.
-
-	Preconditions:
-	maxLevel - minLevel >= 0
-
-	Time complexity:
-	O(log(s - minLevel + 2)),
-	where
-	s is the returned element.
-
-	minLevel, maxLevel:
-	The searched range is [minLevel, maxLevel).
-
-	indicator:
-	An indicator with the property that the
-	return element exists.
-
-	returns:
-	An element s in [minLevel, maxLevel] such that 
-	the indicator is true on [minLevel, s) and 
-	false on [s, maxlevel).
-	*/
-	template <typename Integer, typename Integer_Indicator>
-	Integer exponentialBinarySearch(
 		Integer minLevel, Integer maxLevel,
 		Integer_Indicator indicator);
 
