2df28b595c9200b5572000cba3e743dca70865dc
kaba <kaba@localhost>
2013-10-22 02:02:04 +0300

    Added matlab-concepts.

---
+++ pastel/+pastelsys/integer_concept.m
@@ -0,0 +1,9 @@
+function messageSet = integer_concept(data)
+
+eval(import_pastel)
+
+messageSet = real_concept(data);
+
+if floor(data) ~= ceil(data)
+	messageSet{end + 1} = 'is not integer-valued';
+end
