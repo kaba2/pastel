b788aee44b951e5f26e7a912a63bcd7bc4554fbe
kaba <kaba@localhost>
2013-10-28 16:33:09 +0200

    Added real-matrix and square-matrix concepts.

---
+++ pastel/+pastelsys/real_matrix_concept.m
@@ -0,0 +1,12 @@
+function messageSet = real_matrix_concept(data)
+
+messageSet =  {};
+
+if ~isnumeric(data)
+    messageSet{end + 1} = 'is not of numeric type';
+end
+
+if ~isreal(data)
+	messageSet{end + 1} = 'is not real';
+end
+
