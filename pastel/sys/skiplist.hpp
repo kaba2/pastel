cea3458b913340cff80b20ddeef0187e1ae20665
Kalle Rutanen <kaba@hilvi.org>
2013-11-04 18:16:55 +0200

    Added code for skip-list removal; still need to debug it.

--- pastel/sys/skiplist.hpp
+++ pastel/sys/skiplist.hpp
@@ -7,6 +7,279 @@ namespace Pastel
 {
 
 	template <typename SkipList_Settings>
+	const typename SkipList_Settings::Key& 
+		SkipList<SkipList_Settings>::nodeKey(Node* node) const
+	{
+		ASSERT(node != end_);
+		return ((Data_Node*)node)->key();
+	}
+
+	template <typename SkipList_Settings>
+	void SkipList<SkipList_Settings>::linkSkipLevels(Node* node)
+	{
+		ASSERT(node != end_);
+
+		integer levels = node->levels();
+
+		Node* prev = node;
+		for (integer i = 1;i < levels;++i)
+		{
+			// We will find the predecessors 
+			// at each skip-level...
+			prev = findPrevious(prev, i);
+
+			// ... and then link the 
+			// node between the predecessor and its
+			// successor.
+			Node* next = prev->link(i)[Next];
+			link(prev, node, i);
+			link(node, next, i);
+		}
+	}
+
+	//! Finds the element preceding a given element on a given level.
+	template <typename SkipList_Settings>
+	SkipList_::Node* SkipList<SkipList_Settings>::findPrevious(Node* node, integer level) const
+	{
+		ASSERT(node != end_);
+		ASSERT_OP(level, >=, 1);
+
+		// Find the representative of the 
+		// previous node.
+		node = node->link(0)[Prev]->repr();
+
+		// Find the first node on the left that
+		// links through 'node' on 'level'.
+		while(node != end_ &&
+			node->levels() <= level)
+		{
+			node = node->link(node->levels() - 1)[Prev];
+		}
+
+		return node;
+	}
+
+	//! Returns the number of elements with equal heights.
+	template <typename SkipList_Settings>
+	template <bool Direction>
+	integer SkipList<SkipList_Settings>::equalLevels(Node* node) const
+	{
+		ASSERT(node != end_);
+
+		integer level = node->levels() - 1;
+
+		integer equals = 0;
+		while (true)
+		{
+			Node* next = node->link(level)[Direction];
+			if (next == end_ ||
+				next->levels() != node->levels())
+			{
+				break;
+			}
+
+			node = next;
+			++equals;
+		}
+
+		return equals;
+	}
+
+	//! Finds the middle of elements with equal levels.
+	template <typename SkipList_Settings>
+	SkipList_::Node* SkipList<SkipList_Settings>::findMiddleOfEqualLevels(Node* node)
+	{
+		integer equalsOnLeft = equalLevels<false>(node);
+		integer equalsOnRight = equalLevels<true>(node);
+		integer equals = equalsOnLeft + equalsOnRight + 1;
+		ASSERT_OP(equals, <=, 3);
+
+		if (equals < 3)
+		{
+			// There are no three subsequent nodes
+			// with the same level.
+			return 0;
+		}
+
+		// The skip-list contains three subsequent
+		// elements on 'node's level. 
+		integer level = node->levels() - 1;
+
+		// Find out the middle of those.
+		Node* middle = 0;
+		switch(equalsOnRight)
+		{
+		case 0:
+			middle = node->link(level)[Prev];
+			break;
+		case 1:
+			middle = node;
+			break;
+		case 2:
+			middle = node->link(level)[Next];
+			break;
+		};
+
+		return middle;
+	}
+
+	template <typename SkipList_Settings>
+	SkipList_::Node* SkipList<SkipList_Settings>::nodeLowerBound(
+		const Key& key,
+		const ConstIterator& hint) const
+	{
+		// Get the representative for 'hint'.
+		Node* node = ((Node*)hint.base())->repr();
+
+		bool direction = true;
+		Node* end = end_;
+		if (node != end)
+		{
+			if (Compare()(nodeKey(node), key))
+			{
+				// Failing this invariant means that
+				// Compare() is not a strict weak order;
+				// this is an error in the user's code.
+				const bool isStrictWeakOrder = 
+					!Compare()(key, nodeKey(node));
+				PENSURE(isStrictWeakOrder);
+
+				// The 'key' is to the right of 'hint'. 
+				direction = true;
+			}
+			else if (Compare()(key, nodeKey(node)))
+			{
+				// The 'key' is to the left from 'hint'.
+				direction = false;
+			}
+			else
+			{
+				// The 'key' is equivalent to 'hint'.
+				Node* lowerBound = node;
+				return lowerBound;
+			}
+		}
+
+		// We know that 'node' is a representative.
+		// Therefore it contains level-1 links.
+		integer level = 1;
+		integer minLevel = 1;
+
+		auto validMoveTest = [&](integer level)
+		{
+			Node* next = node->link(level)[direction];
+			// We call 'next' an overshoot, if
+			// 1) it is the sentinel node,
+			// 2) it is not the sentinel node, and 
+			// 'key' is smaller than the key of 'next',
+			// 3) it is not the sentinel node, and
+			// 'key' is equivalent to the key of 'next'.
+			bool overshoots = (next == end ||
+				Compare()(nodeKey(next), key) != direction);
+			return !overshoots;
+		};
+
+		// Ascend towards the key.
+		while(true)
+		{
+			integer overshootLevel = 
+				binarySearch(minLevel, node->levels(), validMoveTest);
+
+			if (overshootLevel == minLevel)
+			{
+				// The link overshoots on all levels up
+				// from 'minLevel'. Start descending.
+				break;
+			}
+
+			minLevel = overshootLevel - 1;
+			node = node->link(minLevel)[direction];
+		}
+
+		// Descend towards the key.
+		integer maxLevel = minLevel;
+		while(true)
+		{
+			integer overshootLevel = 
+				binarySearch((integer)1, maxLevel, validMoveTest);
+
+			if (overshootLevel == 1)
+			{
+				// The link overshoots on all levels up from 1.
+				break;
+			}
+
+			maxLevel = overshootLevel;
+			node = node->link(maxLevel - 1)[direction];
+		}
+
+		if (direction)
+		{
+			// The next node gives the lower-bound.
+			node = node->link(1)[direction];
+		}
+
+		return node;
+	}
+
+	template <typename SkipList_Settings>
+	SkipList_::Node* SkipList<SkipList_Settings>::nodeUpperBound(
+		const Key& key,
+		const ConstIterator& hint) const
+	{
+		Node* lowerBound = nodeLowerBound(key, hint);
+		ASSERT(lowerBound->isRepresentative());
+
+		if (lowerBound == end_)
+		{
+			// If the key does not have a lower-bound,
+			// then it does not have an upper-bound 
+			// either.
+			return end_;
+		}
+		
+		Node* upperBound = lowerBound;
+		if (!Compare()(key, nodeKey(upperBound)))
+		{
+			// The lower bound is equivalent to the
+			// key. Therefore the upper-bound is given 
+			// by the next unique key. Since 
+			// 'lowerBound' is a representative,
+			// it is linked on level 1, which
+			// directly gives the node with the
+			// next unique key.
+			upperBound = upperBound->link(1)[Next];
+		}
+		else
+		{
+			// Since the lower-bound is not equivalent
+			// to the key, it is also an upper-bound.
+		}
+
+		return upperBound;
+	}
+
+	//! Links subsequent nodes together at level i.
+	/*!
+	Time complexity:
+	O(1)
+
+	Exception safety:
+	nothrow
+	*/
+	template <typename SkipList_Settings>
+	void SkipList<SkipList_Settings>::link(Node* left, Node* right, integer i)
+	{
+		left->link(i)[Next] = right;
+		right->link(i)[Prev] = left;
+	}
+
+}
+
+namespace Pastel
+{
+
+	template <typename SkipList_Settings>
 	bool validInvariants(const SkipList<SkipList_Settings>& that)
 	{
 		if (that.empty())
---
+++ pastel/sys/skiplist_insert.hpp
@@ -0,0 +1,308 @@
+// Description: Skip-list element insertion
+
+#ifndef PASTELSYS_SKIPLIST_INSERT_H
+#define PASTELSYS_SKIPLIST_INSERT_H
+
+#include "pastel/sys/skiplist.h"
+
+namespace Pastel
+{
+
+	template <typename SkipList_Settings>
+	typename SkipList<SkipList_Settings>::Iterator 
+		SkipList<SkipList_Settings>::insert(
+		Key key, 
+		Value_Class value = Value_Class())
+	{
+		// Preallocate link-sets of sizes 2^i. This is needed
+		// to achieve strong exception safety, as well as to
+		// avoid additional calls to the memory manager.
+		preallocate();
+
+		// Find the element before which to insert
+		// the new element.
+		Iterator nextIter = upper_bound(key);
+
+		// Check if there already is an equivalent
+		// key in the skip-list.
+		bool keyAlreadyExists = false;
+		if (nextIter != begin())
+		{
+			// Either every element is smaller than the inserted
+			// element, or some are smaller and some not-smaller.
+			// Find out which is the case.
+			Iterator prevNextIter = std::prev(nextIter);
+			keyAlreadyExists = !Compare()(prevNextIter.key(), key);
+		}
+		else
+		{
+			// Since the first element is greater than the 
+			// inserted element, every element is. The
+			// key does not exist in the skip-list.
+		}
+
+		// Create a new node with the given data.
+		std::unique_ptr<Data_Node> nodePtr(
+			new Data_Node(std::move(key), std::move(value)));
+		Data_Node* node = nodePtr.get();
+
+		if (keyAlreadyExists)
+		{
+			// At least one equivalent key exists in the
+			// skip-list already. 
+
+			// The equivalence class is created if and only
+			// if it contains at least two elements.
+
+			Node* prevNext = std::prev(nextIter).base();
+			SuperNode* super = prevNext->super();
+			if (!super)
+			{
+				// Create the equivalence class.
+				// The representative is the first element
+				// in the equivalence class; it is the
+				// 'prevNext'.
+				super = new SuperNode(prevNext);
+				prevNext->super() = super;
+			}
+
+			// Assign the equivalence class to the
+			// new element.
+			node->super() = super;
+			++super->keys();
+		}
+
+		// If an equivalent element already exists in the
+		// skip-list, then we will only link it in the basic
+		// level. Otherwise we will also provide with a
+		// link in the first skip-level.
+		integer i = keyAlreadyExists ? 0 : 1;
+		integer levels = powerOfTwo(i);
+		node->setLinkSet(std::move(allocatedSet_[i]), levels);
+
+		// No exceptions beyond this point.
+		nodePtr.release();
+
+		// Link the basic level.
+		{
+			Node* next = (Node*)nextIter.base();
+			Node* prev = next->link(0)[Prev];
+			link(prev, node, 0);
+			link(node, next, 0);
+		}
+
+		if (!keyAlreadyExists)
+		{
+			// The inserted key is unique to the skip-list.
+			// Link the skip-levels.
+			linkSkipLevels(node);
+			++uniqueKeys_;
+
+			// Rebalance the skip-list.
+			rebalanceInsert(node);
+		}
+		else
+		{
+			// The skip-list contains keys 
+			// which are equivalent to the
+			// inserted key. We will make the
+			// new element invisible to the
+			// skip-levels by not linking them.
+		}
+
+		// Update the size.
+		++size_;
+
+		// Return an iterator to the new node.
+		return Iterator(node);
+	}
+
+	template <typename SkipList_Settings>
+	void SkipList<SkipList_Settings>::rebalanceInsert(Node* node)
+	{
+		ASSERT_OP(node->levels(), ==, 2);
+		while(true)
+		{
+			ASSERT(node != end_);
+			ASSERT(node->isRepresentative());
+
+			Node* middle = findMiddleOfEqualLevels(node);
+			if (!middle)
+			{
+				// All invariants now hold.
+				break;
+			}
+
+			// The skip-list contains three subsequent
+			// elements on 'node's level. 
+			// This breaks the invariant of the deterministic
+			// 1-2-skip-list to have at most two subsequent
+			// elements at a given level at the same level.
+
+			// The invariant is fixed locally by adding one 
+			// logical level to 'middle'.
+			increaseLevel(middle);
+
+			// Adding the level to 'middle' may have 
+			// broken the invariant on the next level.
+			// Recurse to rebalance 'middle'.
+			node = middle;
+		}
+	}
+
+	template <typename SkipList_Settings>
+	void SkipList<SkipList_Settings>::preallocate()
+	{
+		if (!endSet_)
+		{
+			// The 'endSet_' is empty if and only if the skip-list
+			// is empty.
+			ASSERT(empty());
+
+			// The sentinel node is given 3 three levels,
+			// since the first inserted element has to have height 2,
+			// and the sentinel node has to have height one
+			// more than then the highest element.
+			integer levels = 3;
+
+			// The allocated amounts are always of the form 2^i.
+			LinkSet linkSet(new Link[4]);
+
+			// Store the one-level sentinel link-set 
+			// to wait for clear().
+			endSet_ = std::move(end_->linkSet_);
+
+			// Link the sentinel node to itself on all
+			// levels.
+			end_->setLinkSet(std::move(linkSet), levels);
+			for (integer i = 0;i < levels;++i)
+			{
+				link(end_, end_, i);
+			}
+		}
+
+		// Make sure the pre-allocated set
+		// can always extend the sentinel node.
+		//
+		// Let m be the size of the `allocatedSet_`,
+		// and h be the height of the sentinel node.
+		// Then h can be extended if
+		//
+		// 		2^{m - 1} >= h + 1
+		//  <=> m - 1 >= log_2(h + 1)
+		//  <=> m >= log_2(h + 1) + 1
+		//  <=  m >= ceil(log_2(h + 1)) + 1
+		//
+		// h  | h + 1 | ceil(log_2(h + 1)) | 2^[ceil(log_2(h + 1))]
+		// -- | ----- | ------------------ | ----------------------
+		// 0  |     1 |                  0 |                      1
+		// 1  |     2 |                  1 |                      2
+		// 2  |     3 |                  2 |                      4
+		// 3  |     4 |                  2 |                      4
+		// 4  |     5 |                  3 |                      8
+		// 5  |     6 |                  3 |                      8
+		// 6  |     7 |                  3 |                      8
+		// 7  |     8 |                  3 |                      8
+		// 8  |     9 |                  4 |                     16
+
+		integer m = allocatedSet_.size();
+		integer h = end_->levels();
+
+		// The height of the link-array at allocatedSet_[i] is 2^i.
+		if (allocatedSet_.empty() || 
+			powerOfTwo(m - 1) < h + 1)
+		{
+			m = integerCeilLog2(h + 1) + 1;
+			allocatedSet_.resize(m);
+		}
+
+		// Preallocate all missing sizes of skip-link arrays.
+		for (integer i = 0;i < m;++i)
+		{
+			if (!allocatedSet_[i])
+			{
+				integer levels = powerOfTwo(i);
+				allocatedSet_[i].reset(new Link[levels]);
+			}
+		}
+	}
+
+	template <typename SkipList_Settings>
+	void SkipList<SkipList_Settings>::increaseLevel(Node* node)
+	{
+		// A given node has a physical size of the form 2^i.
+		// If the number of levels in a node already is of
+		// this form, then we need to double the physical size.
+
+		integer n = node->levels();
+
+		// Find the node which will link to 'node' on 
+		// the new level.
+		Node* prev = end_;
+		Node* next = end_;
+		if (node != end_)
+		{
+			prev = findPrevious(node, n);
+			next = prev->link(n)[Next];
+			if (prev == end_)
+			{
+				// The link comes from the sentinel node.
+				// Make sure that the sentinel node has the
+				// necessary amount of levels.
+				ASSERT_OP(n, <, end_->levels());
+				if (n == end_->levels() - 1)
+				{
+					// This implies that the sentinel node
+					// always has the greatest amount of
+					// levels.
+					increaseLevel(end_);
+				}
+			}
+		}
+
+		if (isPowerOfTwo(n))
+		{
+			// The node needs to be physically resized
+			// to increase a level.
+
+			// Find out the index `i` such that 
+			// `allocatedSet_[i]` is a link-set
+			// of size 2 * n.
+			//
+			//		2^i = 2n
+			// <=>  i = log_2(n) + 1
+			integer i = integerLog2(n) + 1;
+			ASSERT_OP(i, <, allocatedSet_.size());
+
+			// This function attains a nothrow exception
+			// safety because it uses the preallocated memory,
+			// rather than allocating the memory now.
+			LinkSet& newLinkSet = allocatedSet_[i];
+			ASSERT(newLinkSet);
+
+			// Copy the links into the new link-set.
+			copy_n(node->linkSet_.get(), n, newLinkSet.get());
+
+			// Possibly return the previously allocated memory.
+			if (node->linkSet_ && 
+				i > 0 && 
+				!allocatedSet_[i - 1])
+			{
+				allocatedSet_[i - 1] = std::move(node->linkSet_);
+			}
+
+			// Move the new link-set into the node.
+			node->linkSet_ = std::move(newLinkSet);
+		}
+
+		// Increase the level of the node.
+		++node->levels_;
+
+		// Link the 'node' on the new level.
+		link(prev, node, n);
+		link(node, next, n);
+	}
+
+}
+
+#endif
