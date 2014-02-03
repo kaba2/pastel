7cd2e5816b01d7c08993018eb711ea1ea7af5d8a
Kalle Rutanen <kaba@hilvi.org>
2014-02-03 15:48:28 +0200

    Made flipping leading bits portable.

--- pastel/sys/flip_leading_zero_bits.h
+++ pastel/sys/flip_leading_zero_bits.h
@@ -4,13 +4,20 @@
 #ifndef PASTELSYS_FLIP_LEADING_ZERO_BITS_H
 #define PASTELSYS_FLIP_LEADING_ZERO_BITS_H
 
-#include "pastel/sys/integer_concept.h"
+#include "pastel/sys/finite_integer_concept.h"
+
+#include <type_traits>
 
 namespace Pastel
 {
 
 	//! Sets the bits below the first 1-bit.
 	/*!
+	Preconditions:
+	Finite_Integer is not a signed integer.
+	This is required to avoid portability issues
+	with bitwise operations on signed integers.
+
 	Time complexity: constant, provided the basic
 	operations on integers are.
 
@@ -19,8 +26,9 @@ namespace Pastel
 	has the leading zero bits changed to 1;
 	x..x10..0 --> x..x11..1
 	*/
-	template <typename Integer>
-	Integer flipLeadingZeroBits(const Integer& that);
+	template <typename Finite_Integer>
+	PASTEL_DISABLE_IF(std::is_signed<Finite_Integer>, Finite_Integer) 
+		flipLeadingZeroBits(const Finite_Integer& that);
 
 }
 
