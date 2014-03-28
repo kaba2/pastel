3462c13c4cbf38bee724d44bc8de00587152b296
Kalle Rutanen <kaba@hilvi.org>
2014-03-28 12:52:53 +0200

    Added a quantile function for the red-black tree.

--- pastel/sys/redblacktree.h
+++ pastel/sys/redblacktree.h
@@ -1183,6 +1183,26 @@ namespace Pastel
 	template <typename Settings, template <typename> class Customization>
 	bool testInvariants(const RedBlackTree<Settings, Customization>& tree);
 
+	//! Returns the alpha-quantile of the elements.
+	/*!
+	Time complexity:
+	O(log(tree.size())), if the alpha-quantile is not cbegin() or clast(),
+	O(1), otherwise
+
+	returns:
+	An element with the index closest to floor(alpha * tree.size()). This 
+	corresponds to assigning an element with an index i the quantile 
+	f(i) = (i + 0.5) / tree.size(), and then returning the element with
+	the closest quantile. For example, if the elements are [a, b, c, d, e],
+	then (-inf, 0.2) ==> a, [0.2, 0.4) ==> b, [0.4, 0.6) ==> c, [0.6, 0.8) ==> d,
+	[0.8, inf) ==> e.
+	*/
+	template <typename Settings, template <typename> class Customization>
+	typename RedBlackTree<Settings, Customization>::ConstIterator
+	quantile(
+		const RedBlackTree<Settings, Customization>& tree,
+		real alpha);
+
 }
 
 namespace Pastel
@@ -1272,5 +1292,6 @@ namespace Pastel
 #include "pastel/sys/redblacktree_splice.hpp"
 #include "pastel/sys/redblacktree_split.hpp"
 #include "pastel/sys/redblacktree_swap.hpp"
+#include "pastel/sys/redblacktree_quantile.hpp"
 
 #endif
