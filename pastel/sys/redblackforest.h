111ec121258cd04e816b174b9fd6f3f9f9271207
kaba <kaba@localhost>
2014-05-08 12:33:15 +0300

    Working on red-black forests.

--- pastel/sys/redblackforest.h
+++ pastel/sys/redblackforest.h
@@ -5,7 +5,7 @@
 
 #include "pastel/sys/redblackforest_concepts.h"
 #include "pastel/sys/redblackforest_fwd.h"
-#include "pastel/sys/redblackforest_set.h"
+#include "pastel/sys/redblacktree.h"
 
 namespace Pastel
 {
@@ -23,9 +23,6 @@ namespace Pastel
 		using Settings = Settings_;
 		using Fwd = RedBlackForest_Fwd<Settings>;
 
-		PASTEL_FWD(Key);
-		PASTEL_FWD(Data_Class);
-
 		PASTEL_FWD(ElementSet);
 		PASTEL_FWD(Element_Iterator);
 		PASTEL_FWD(Element_ConstIterator);
@@ -36,22 +33,66 @@ namespace Pastel
 
 		PASTEL_FWD(Iterator);
 		PASTEL_FWD(ConstIterator);
+		PASTEL_FWD(Range);
+		PASTEL_FWD(ConstRange);
 
 		using Customization = Customization_<Settings>;
 
+		//! Constructs an empty forest.
+		/*!
+		Time complexity: O(1)
+		Exception safety: strong
+		*/
 		RedBlackForest()
 		: setSet_()
-		, size_(0)
 		{
 			// Create the first set.
-			setSet_.emplace_back();
+			insert(setSet_.cend());
 			// Create the last set.
-			setSet_.emplace_back();
+			insert(setSet_.cend());
 		}
 
-		RedBlackForest(const RedBlackForest& that) = delete;
-		RedBlackForest(RedBlackForest&& that) = delete;
-		RedBlackForest& operator=(RedBlackForest) = delete;
+		//! Copy-constructs from another forest.
+		/*!
+		Time complexity: O(that.sets() + that.size())
+		Exception safety: strong
+		*/
+		RedBlackForest(const RedBlackForest& that)
+		: RedBlackForest()
+		{
+			for (auto&& set : that.csetRange())
+			{
+				setSet_.emplace(csetEnd(), set);
+			}
+		}
+
+		//! Move-constructs from another forest.
+		/*!
+		Time complexity: O(1)
+		Exception safety: strong
+		*/
+		RedBlackForest(RedBlackForest&& that)
+		: RedBlackForest()
+		{
+			swap(that);
+		}
+		
+		//! Assigns from another forest.
+		/*!
+		Time complexity: O(that.sets() + that.size())
+		Exception safety: strong
+		*/
+		RedBlackForest& operator=(RedBlackForest that)
+		{
+			swap(that);
+			return *this;
+		}
+
+		//! Destructs a forest.
+		/*!
+		Time complexity: O(that.set() + that.size())
+		Exception safety: nothrow
+		*/
 		~RedBlackForest() = default;
 
 		//! Removes all elements and sets.
@@ -67,8 +108,6 @@ namespace Pastel
 			{
 				set = eraseSet(set);
 			}
-
-			size_ = 0;
 		}
 
 		//! Swaps two forests.
@@ -79,17 +118,16 @@ namespace Pastel
 		void swap(RedBlackForest& that)
 		{
 			setSet_.swap(that.setSet_);
-			std::swap(size_, that.size_);
 		}
 
-		//! Inserts an empty last set into the forest.
+		//! Inserts an empty set at the end of the forest.
 		/*!
-		Time complexity: O(1)
-		Exception safety: strong
+		This is a convenience function which calls
+		insertSet(csetEnd()).
 		*/
-		Set_Iterator insertSet()
+		Set_Iterator insert()
 		{
-			return insertSet(csetEnd());
+			return insert(csetEnd());
 		}
 
 		//! Inserts an empty set into the forest.
@@ -97,10 +135,12 @@ namespace Pastel
 		Time complexity: O(1)
 		Exception safety: strong
 		*/
-		Set_Iterator insertSet(
+		Set_Iterator insert(
 			const Set_ConstIterator& before)
 		{
-			return setSet_.emplace(before);
+			Set_Iterator set = setSet_.emplace(before);
+			set->end().sentinelData().set = set;
+			return set;
 		}
 
 		//! Removes a set from the forest.
@@ -108,59 +148,9 @@ namespace Pastel
 		Time complexity: O(set->size())
 		Exception safety: nothrow
 		*/
-		Set_Iterator eraseSet(const Set_ConstIterator& set)
+		Set_Iterator erase(const Set_ConstIterator& set)
 		{
 			return setSet_.erase(set);
-		}
-
-		//! Splits a set into two along the given key.
-		/*!
-		Time complexity: O(set->size())
-		Exception safety: strong
-		*/
-		Set_Iterator split(
-			const Set_ConstIterator& set,
-			const Key& key)
-		{
-			Set_Iterator nextSet;
-
-			// Split the element-set of 'set'.
-			ElementSet nextElementSet = cast(set)->split(key);
-
-			try
-			{
-				// Create a new set.
-				nextSet = insertSet(set);
-
-				// Move the splitted element-set into the new set.
-				nextSet->elementSet_ = std::move(nextElementSet);
-			}
-			catch(...)
-			{
-				// Roll back the set-split.
-				cast(set)->join(nextElementSet);
-				throw;
-			};
-
-			// Return the new set.
-			return nextSet;
-		}
-
-		//! Joins the elements in 'fromSet' to 'toSet'.
-		/*!
-		Time complexity: 
-		O(log(n_1 + 2) - log(n_2 + 2))
-		where
-		n_1 = max(fromSet->size(), toSet->size()), and
-		n_2 = min(fromSet->size(), toSet->size()).
-
-		Exception safety: nothrow
-		*/
-		void join(
-			const Set_ConstIterator& fromSet,
-			const Set_ConstIterator& toSet)
-		{
-			cast(toSet)->join(cast(fromSet)->elementSet_);
 		}
 
 		//! Finds the set that contains a given element.
@@ -172,7 +162,7 @@ namespace Pastel
 
 		Exception safety: nothrow
 		*/
-		Set_ConstIterator findSet(
+		Set_ConstIterator find(
 			const ConstIterator& element) const
 		{
 			while(!element.isSentinel())
@@ -188,10 +178,10 @@ namespace Pastel
 		This is a convenience function which returns
 		cast(addConst(*this).findSet(element));
 		*/
-		Set_Iterator findSet(
+		Set_Iterator find(
 			const ConstIterator& element)
 		{
-			return cast(addConst(*this).findSet(element));
+			return cast(addConst(*this).find(element));
 		}
 
 		//! Returns the number of sets in the forest.
@@ -202,118 +192,6 @@ namespace Pastel
 		integer sets() const
 		{
 			return setSet_.size();
-		}
-
-		//! Inserts an element into a set.
-		/*!
-		Time complexity: O(log(set->size() + 2))
-		Exception safety: strong
-		*/
-		std::pair<Iterator, bool> insert(
-			const Set_ConstIterator& set,
-			const Key& key, 
-			const Data_Class& data = Data_Class())
-		{
-			auto result = cast(set)->insert(key, data);
-			++size_;
-			return result;
-		}
-
-		//! Removes an element from a set.
-		/*!
-		Time complexity: O(log(set->size() + 2))
-		Exception safety: nothrow
-		*/
-		Iterator erase(
-			const ConstIterator& element)
-		{
-			Set_Iterator set = findSet(element);
-			--size_;
-			return set->erase(element);
-		}
-
-		//! Returns whether a set contains a given key.
-		/*!
-		Time complexity: O(log(set->size()))
-		Exception safety: nothrow
-		*/
-		bool exists(
-			const Set_ConstIterator& set,
-			const Key& key) const
-		{
-			return set->exists(key);
-		}
-
-		//! Searches for an element with a given key.
-		/*!
-		Time complexity: O(log(set->size()))
-		Exception safety: nothrow
-		*/
-		ConstIterator find(
-			const Set_ConstIterator& set,
-			const Key& key) const
-		{
-			return set->find(key);
-		}
-
-		//! Searches for an element with a given key.
-		/*!
-		This is a convenience function which calls
-		cast(addConst(*this).find(set, key));
-		*/
-		Iterator find(
-			const Set_ConstIterator& set,
-			const Key& key)
-		{
-			return cast(addConst(*this).find(set, key));
-		}
-
-		//! Returns the smallest element >= key.
-		/*!
-		Time complexity: O(log(set->size()))
-		Exception safety: nothrow
-		*/
-		ConstIterator lowerBound(
-			const Set_ConstIterator& set,
-			const Key& key) const
-		{
-			return set->lowerBound(key);
-		}
-
-		//! Returns the smallest element >= key.
-		/*!
-		This is a convenience function which calls
-		cast(addConst(*this).lowerBound(set, key));
-		*/
-		Iterator lowerBound(
-			const Set_ConstIterator& set,
-			const Key& key)
-		{
-			return cast(addConst(*this).lowerBound(set, key));
-		}
-
-		//! Returns the smallest element > key.
-		/*!
-		Time complexity: O(log(set->size()))
-		Exception safety: nothrow
-		*/
-		ConstIterator upperBound(
-			const Set_ConstIterator& set,
-			const Key& key) const
-		{
-			return set->upperBound(key);
-		}
-
-		//! Returns the smallest element > key.
-		/*!
-		This is a convenience function which calls
-		cast(addConst(*this).upperBound(set, key));
-		*/
-		Iterator upperBound(
-			const Set_ConstIterator& set,
-			const Key& key)
-		{
-			return cast(addConst(*this).upperBound(set, key));
 		}
 
 		//! Removes constness from an iterator.
@@ -336,26 +214,6 @@ namespace Pastel
 			return setSet_.erase(that, that);
 		}
 
-		//! Returns the number of elements in the forest.
-		/*!
-		Time complexity: O(1)
-		Exception safety: nothrow
-		*/
-		integer size() const
-		{
-			return size_;
-		}
-
-		//! Returns whether there are elements in the forest.
-		/*!
-		Time complexity: O(1)
-		Exception safety: nothrow
-		*/
-		bool empty() const
-		{
-			return size_ == 0;
-		}
-
 		PASTEL_ITERATOR_FUNCTIONS_PREFIX(Set_, setBegin, std::next(setSet_.begin()));
 		PASTEL_ITERATOR_FUNCTIONS_PREFIX(Set_, setEnd, std::prev(setSet_.end()));
 		PASTEL_ITERATOR_FUNCTIONS_PREFIX(Set_, setLast, std::prev(setEnd()));
@@ -363,10 +221,10 @@ namespace Pastel
 		PASTEL_ITERATOR_FUNCTIONS(begin, setBegin()->begin());
 		PASTEL_ITERATOR_FUNCTIONS(end, setEnd()->end());
 		PASTEL_ITERATOR_FUNCTIONS(last, setEnd()->last());
+		PASTEL_RANGE_FUNCTIONS(range, begin, end);
 
 	private:
 		SetSet setSet_;
-		integer size_;
 	};
 
 }
@@ -374,78 +232,22 @@ namespace Pastel
 namespace Pastel
 {
 
-	template <
-		typename Key_, 
-		typename Data_ = void,
-		typename Less_ = LessThan,
-		typename Propagation_ = void,
-		typename SentinelData_ = void,
-		bool MultipleKeys_ = false>
+	template <typename RedBlackTree_>
 	class RedBlackForest_Settings
 	{
 	public:
-		using Key = Key_;
-		using Data = Data_;
-		using Less = Less_;
-		using Propagation = Propagation_;
-		using SentinelData = SentinelData_;
-		PASTEL_CONSTEXPR bool MultipleKeys = MultipleKeys_;
+		using ElementSet = RedBlackTree_;
 	};
 
 }
 
-// Map
-
 namespace Pastel
 {
 
-	template <
-		typename Key, 
-		typename Data,
-		typename Less = LessThan,
-		typename Propagation = void,
-		typename SentinelData = void,
-		template <typename> class Customization = Empty_RedBlackForest_Customization>
-	using RedBlackForest_Map = 
-		RedBlackForest<RedBlackForest_Settings<Key, Data, Less, Propagation, SentinelData, false>, 
-		Customization>;
-
-	template <
-		typename Key, 
-		typename Data,
-		typename Less = LessThan,
-		typename Propagation = void,
-		typename SentinelData = void,
-		template <typename> class Customization = Empty_RedBlackForest_Customization>
-	using RedBlackForest_MultiMap = 
-		RedBlackForest<RedBlackForest_Settings<Key, Data, Less, Propagation, SentinelData, true>, 
-		Customization>;
-
-}
-
-// Set
-
-namespace Pastel
-{
-
-	template <
-		typename Key,
-		typename Less = LessThan,
-		typename Propagation = void,
-		typename SentinelData = void,
+	template <typename RedBlackTree_,
 		template <typename> class Customization = Empty_RedBlackForest_Customization>
 	using RedBlackForest_Set = 
-		RedBlackForest<RedBlackForest_Settings<Key, void, Less, Propagation, SentinelData, false>, 
-		Customization>;
-
-	template <
-		typename Key, 
-		typename Less = LessThan,
-		typename Propagation = void,
-		typename SentinelData = void,
-		template <typename> class Customization = Empty_RedBlackForest_Customization>
-	using RedBlackForest_MultiSet = 
-		RedBlackForest<RedBlackForest_Settings<Key, void, Less, Propagation, SentinelData, true>, 
+		RedBlackForest<RedBlackForest_Settings<RedBlackTree_>, 
 		Customization>;
 
 }
--- pastel/sys/redblackforest_fwd.h
+++ pastel/sys/redblackforest_fwd.h
@@ -4,6 +4,7 @@
 #define PASTELSYS_REDBLACKFOREST_FWD_H
 
 #include "pastel/sys/redblacktree.h"
+#include "pastel/sys/redblackforest_iterator.h"
 
 #include <list>
 
@@ -13,38 +14,54 @@ namespace Pastel
 	template <typename, template <typename> class>
 	class RedBlackForest;
 
-	namespace RedBlackForest_
-	{
-
-		template <typename>
-		class Set;
-
-	}
-
 	template <typename Settings>
 	class RedBlackForest_Fwd
 	{
 	public:
 		using Fwd = Settings;
-		
-		PASTEL_FWD(Key);
-		PASTEL_FWD(Data);
-		PASTEL_FWD(Less);
 
-		using Data_Class = Class<Data>;
+		using User_ElementSet = typename Settings::ElementSet;
+		using User_SentinelData = typename User_ElementSet::Settings::SentinelData;
 
-		using ElementSet = RedBlack_Map<Key, Data, Less>;
+		class ElementSet_Settings;
+		using ElementSet = RedBlackTree<ElementSet_Settings>;
+
+		using SetSet = std::list<ElementSet>;
+		using Set_Iterator = typename SetSet::iterator;
+		using Set_ConstIterator = typename SetSet::const_iterator;
+
+		class ElementSet_SentinelData
+		: public Class<User_SentinelData>
+		{
+		private:
+			template <typename, template <typename> class>
+			friend class RedBlackForest;
+
+			friend class ConstIterator;
+			friend class Iterator;
+
+			Set_Iterator set;
+		};
+
+		class ElementSet_Settings
+		: public User_ElementSet::Settings
+		{
+		public:
+			// Override the sentinel data, but preserve everything else.
+			using SentinelData = ElementSet_SentinelData;
+		};
+
 		using Element_Iterator = typename ElementSet::Iterator;
 		using Element_ConstIterator = typename ElementSet::ConstIterator;
 
-		using Iterator = Element_Iterator;
-		using ConstIterator = Element_ConstIterator;
+		using ConstIterator = RedBlackForest_::Continuous_Iterator<Element_ConstIterator>;
+		using Iterator = RedBlackForest_::Continuous_Iterator<Element_Iterator>;
+		using Range = boost::iterator_range<Iterator>;
+		using ConstRange = boost::iterator_range<ConstIterator>;
 
-		using Set = RedBlackForest_::Set<Settings>;
-
-		using SetSet = std::list<Set>;
-		using Set_Iterator = typename SetSet::iterator;
-		using Set_ConstIterator = typename SetSet::const_iterator;
+		// These are for compatibility with Boost ranges.
+		using const_iterator = ConstIterator;
+		using iterator = Iterator;
 	};
 
 }
