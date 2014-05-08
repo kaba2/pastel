36fd3148c2662a34acce5429737717f613840698
kaba <kaba@localhost>
2014-05-08 14:25:41 +0300

    Worked on red-black forests.

--- pastel/sys/redblackforest_fwd.h
+++ pastel/sys/redblackforest_fwd.h
@@ -20,17 +20,17 @@ namespace Pastel
 	public:
 		using Fwd = Settings;
 
-		using User_ElementSet = typename Settings::ElementSet;
-		using User_SentinelData = typename User_ElementSet::Settings::SentinelData;
+		using User_Tree = typename Settings::Tree;
+		using User_SentinelData = typename User_Tree::Settings::SentinelData;
 
-		class ElementSet_Settings;
-		using ElementSet = RedBlackTree<ElementSet_Settings>;
+		class Tree_Settings;
+		using Tree = RedBlackTree<Tree_Settings>;
 
-		using SetSet = std::list<ElementSet>;
-		using Set_Iterator = typename SetSet::iterator;
-		using Set_ConstIterator = typename SetSet::const_iterator;
+		using TreeSet = std::list<Tree>;
+		using Tree_Iterator = typename TreeSet::iterator;
+		using Tree_ConstIterator = typename TreeSet::const_iterator;
 
-		class ElementSet_SentinelData
+		class Tree_SentinelData
 		: public Class<User_SentinelData>
 		{
 		private:
@@ -40,19 +40,19 @@ namespace Pastel
 			friend class ConstIterator;
 			friend class Iterator;
 
-			Set_Iterator set;
+			Tree_Iterator tree;
 		};
 
-		class ElementSet_Settings
-		: public User_ElementSet::Settings
+		class Tree_Settings
+		: public User_Tree::Settings
 		{
 		public:
 			// Override the sentinel data, but preserve everything else.
-			using SentinelData = ElementSet_SentinelData;
+			using SentinelData = Tree_SentinelData;
 		};
 
-		using Element_Iterator = typename ElementSet::Iterator;
-		using Element_ConstIterator = typename ElementSet::ConstIterator;
+		using Element_Iterator = typename Tree::Iterator;
+		using Element_ConstIterator = typename Tree::ConstIterator;
 
 		using ConstIterator = RedBlackForest_::Continuous_Iterator<Element_ConstIterator>;
 		using Iterator = RedBlackForest_::Continuous_Iterator<Element_Iterator>;
