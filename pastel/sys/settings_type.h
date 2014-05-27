b52dcb68c813c8afe4516c7af231d03fded5b640
Kalle Rutanen <kaba@hilvi.org>
2014-05-28 01:27:19 +0300

    Added Replace_Settings.

--- pastel/sys/settings_type.h
+++ pastel/sys/settings_type.h
@@ -25,9 +25,32 @@ namespace Pastel
 
 	}
 
+	namespace Settings_Type_
+	{
+
+		template <typename Type, typename New_Settings>
+		class Replace_Settings;
+
+		template <
+			template <typename, template <typename> class> class Template,
+			typename Settings_,
+			template <typename> class Customization_,
+			typename New_Settings>
+		class Replace_Settings<Template<Settings_, Customization_>, New_Settings>
+		{
+		public:
+			using type = Template<New_Settings, Customization_>;
+		};
+
+	}
+
 	template <typename Type>
 	using Settings_For = typename Settings_Type_::Settings<Type>::type;
 
+	template <
+		typename Type, 
+		typename New_Settings>
+	using Replace_Settings = typename Settings_Type_::Replace_Settings<Type, New_Settings>::type;
 
 }
 
