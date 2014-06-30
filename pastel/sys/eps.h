1904d64300bc1793fd1ce285725af2dd4a0e1381
Kalle Rutanen <kaba@hilvi.org>
2014-06-30 18:51:48 +0300

    Updated documentation.

--- pastel/sys/eps.h
+++ pastel/sys/eps.h
@@ -11,19 +11,32 @@ namespace Pastel
 
 	//! Returns the next greater 64-bit floating point value.
 	/*!
-	Postcondition:
-	that < nextGreater(that),
-	if 'that' is not +infinity or NaN.
+	returns:
+	The smallest floating point value > that, unless
+	'that' is -infinity, +infinity, or NaN, in which
+	itself.
 	*/
 	real64_ieee nextGreater(real64_ieee that);
 
 	//! Returns the next smaller 64-bit floating point value.
+	/*!
+	returns:
+	The greatest floating point value < that, unless
+	'that' is -infinity, +infinity, or NaN, in which
+	itself.
+	*/
 	real64_ieee nextSmaller(real64_ieee that);
 
 	//! Returns the next greater 32-bit floating point value.
+	/*!
+	See the documentation for the 64-bit version.
+	*/
 	real32_ieee nextGreater(real32_ieee that);
 
 	//! Returns the next smaller 32-bit floating point value.
+	/*!
+	See the documentation for the 64-bit version.
+	*/
 	real32_ieee nextSmaller(real32_ieee that);
 
 }
