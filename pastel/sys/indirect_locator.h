38678f6c7448992a819db22734d5f4a0de8b5007
kaba <kaba@localhost>
2014-07-18 21:53:57 +0200

    Added indirect and transform locators.

---
+++ pastel/sys/indirect_locator.h
@@ -0,0 +1,59 @@
+// Description: Indirect locator
+// Documentation: locators.txt
+
+#ifndef PASTELSYS_INDIRECT_LOCATOR_H
+#define PASTELSYS_INDIRECT_LOCATOR_H
+
+#include "pastel/sys/locator_concept.h"
+
+namespace Pastel
+{
+
+	template <
+		typename Point_,
+		typename Locator>
+	class Indirect_Locator
+	{
+	public:
+		static PASTEL_CONSTEXPR int N = Locator::N;
+		using Real = typename Locator::Real;
+		using Point = Point_;
+
+		explicit Indirect_Locator(
+			const Locator& locator)
+			: locator_(locator)
+		{
+		}
+
+		void swap(Indirect_Locator& that)
+		{
+			locator_.swap(that.locator_);
+		}
+
+		integer n() const
+		{
+			return locator_.n();
+		}
+
+		const Real& operator()(
+			const Point& point, integer i) const
+		{
+			return locator_(*point, i);
+		}
+
+	private:
+		Locator locator_;
+	};
+
+	template <
+		typename Point,
+		typename Locator>
+	Indirect_Locator<Point, Locator> indirectLocator(
+		const Locator& locator)
+	{
+		return Indirect_Locator<Point, Locator>(locator);
+	}
+
+}
+
+#endif
--- pastel/sys/locators.h
+++ pastel/sys/locators.h
@@ -5,7 +5,9 @@
 
 #include "pastel/sys/array_locator.h"
 #include "pastel/sys/default_locator.h"
+#include "pastel/sys/indirect_locator.h"
 #include "pastel/sys/pointer_locator.h"
+#include "pastel/sys/transform_locator.h"
 #include "pastel/sys/vector_locator.h"
 
 #endif
