88c05de43dbf18498b3497945f90a40e45bceaf4
kaba <kaba@localhost>
2014-07-23 02:18:08 +0200

    Updated aggregate files.

--- pastel/sys/generic_programming.h
+++ pastel/sys/generic_programming.h
@@ -5,9 +5,11 @@
 
 #include "pastel/sys/algorithm_objects.h"
 #include "pastel/sys/directors.h"
+#include "pastel/sys/indicators.h"
 #include "pastel/sys/inputs.h"
 #include "pastel/sys/iterators.h"
 #include "pastel/sys/outputs.h"
 #include "pastel/sys/read_protected.h"
+#include "pastel/sys/trindicators.h"
 
 #endif
