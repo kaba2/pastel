eab7c298465e63269534887c72e7b4f563322e9d
kaba <kaba@localhost>
2014-08-27 01:22:30 +0300

    Towards header-only...

--- pastel/gfx/mitchell_filter.h
+++ pastel/gfx/mitchell_filter.h
@@ -8,6 +8,8 @@
 #include "pastel/gfx/gfxlibrary.h"
 #include "pastel/gfx/filter.h"
 
+#include <cmath>
+
 namespace Pastel
 {
 
@@ -20,7 +22,7 @@ namespace Pastel
 	This filter is interpolatory if and only if B = 0.
 	However, if you want interpolation, then consider using
 	Cubic_Filter which is specialized for this case and
-	thus more efficient.
+	is thus more efficient.
 
 	The default	values comes from the
 	experiments of Mitchell and Netravali where they
@@ -28,7 +30,7 @@ namespace Pastel
 	on the line 2C + B = 1. They further deemed
 	(B = 1/3, C = 1/3) as the best from this line.
 	*/
-	class PASTELGFX Mitchell_Filter
+	class Mitchell_Filter
 		: public Filter
 	{
 	public:
@@ -37,10 +39,57 @@ namespace Pastel
 
 		explicit Mitchell_Filter(
 			real b = (real)1 / 3,
-			real c = (real)1 / 3);
-		virtual ~Mitchell_Filter();
+			real c = (real)1 / 3)
+		: Filter(2, "mitchell")
+		, b_(b)
+		, c_(c)
+		{
+		}
+		
+		virtual ~Mitchell_Filter()
+		{
+		}
 
-		virtual real evaluateInRange(real x) const;
+		virtual real evaluateInRange(real x) const
+		{
+			// Let
+			// f(x) = ax^3 + bx^2 + cx + d
+			// g(x) = a'x^3 + b'x^2 + c'x + d'
+			//
+			// Now require:
+			// f'(0) = 0
+			// f'(1) = g'(1)
+			// g(2) = 0
+			// for all x: sum[i=-oo..oo] (f(x) + f(1 - x) + g(x + 1) + g(2 - x)) = 1
+			//
+			// For the last equation, one gets four equations from the fact that
+			// if a polynomial is to be zero for all x, then it must be the zero
+			// polynomial which means that the coefficients must be zero.
+			//
+			// These give seven equations for 8 unknowns.
+			// Represent the equations in matrix form:
+			// Mx = b
+			// and then reduce
+			// [M | b] to reduced row echelon form to
+			// find the 2-parameter family of
+			// Mitchell-Netravali cubic splines
+			// with B and C as the parameter.
+
+			real xAbs = mabs(x);
+
+			if (xAbs < 1)
+			{
+
+				return ((12 - 9 * b_ - 6 * c_) * xAbs +
+					(-18 + 12 * b_ + 6 * c_)) * xAbs * xAbs +
+					(6 - 2 * b_);
+			}
+
+			return (((-b_ - 6 * c_) * xAbs +
+				(6 * b_ + 30 * c_)) * xAbs +
+				(-12 * b_ - 48 * c_)) * xAbs +
+				(8 * b_ + 24 * c_);
+		}
 
 	private:
 		Mitchell_Filter(const Mitchell_Filter& that) = delete;
@@ -57,7 +106,7 @@ namespace Pastel
 		real b = (real)1 / 3,
 		real c = (real)1 / 3)
 	{
-		return MitchellFilterPtr(new Mitchell_Filter(b, c));
+		return std::make_shared<Mitchell_Filter>(b, c);
 	}
 
 }
