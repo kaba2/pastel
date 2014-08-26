b5490516e525ac14fc4197263742dde85434042a
kaba <kaba@localhost>
2014-08-27 01:03:03 +0300

    Generalized sphere volume computations.

--- pastel/geometry/sphere_volume.hpp
+++ pastel/geometry/sphere_volume.hpp
@@ -16,17 +16,98 @@ namespace Pastel
 	{
 		PENSURE_OP(n, >, 0);
 
+		// See volumeUnitSphereManhattan() for the derivation 
+		// of the volume.
+
+		// ln(volume(S))
+		// = ln(2^n / n!)
+		// = n ln(2) - ln(n!)
+
 		return n * constantLn2<Real>() - lnFactorial<Real>(n);
 	}
+
+	template <typename Real>
+	Real volumeUnitSphereManhattan(integer n)
+	{
+		ENSURE_OP(n, >, 0);
+
+		// The volume of Manhattan unit sphere is
+		// 2^n / n!.
+
+		switch(n)
+		{
+		case 1:
+			// 2^1 / 1! = 2
+			return 2;
+			break;
+		case 2:
+			// 2^2 / 2! = 2
+			return 2;
+			break;
+		case 3:
+			// 2^3 / 3! = 8 / 6 = 4 / 3
+			return (Real)4 / 3;
+			break;
+		};
+
+		return std::pow((Real)2, (Real)n) / gamma<Real>(n + 1);
+	} 
 
 	template <typename Real>
 	Real lnVolumeUnitSphere(integer n)
 	{
 		ENSURE_OP(n, >, 0);
 
+		// See volumeUnitSphere() for the derivation 
+		// of the volume.
+
+		// ln(volume(S))
+		// = ln(pi^(n / 2) / gamma(n / 2 + 1))
+		// = (n / 2) ln(pi) - ln(gamma(n / 2 + 1))
+
 		Real nHalf = (Real)n / 2;
 		return nHalf * std::log(constantPi<Real>()) - lnGamma<Real>(nHalf + 1);
 	}
+
+	template <typename Real>
+	Real volumeUnitSphere(integer n)
+	{
+		ENSURE_OP(n, >, 0);
+
+		// The volume of the Euclidean unit sphere is
+		// pi^(n / 2) / gamma(n / 2 + 1).
+
+		switch(n)
+		{
+		case 1:
+			// pi^(0.5) / gamma(1.5) 
+			// = pi^(0.5) / (0.5 gamma(0.5))
+			// = pi^(0.5) / (0.5 pi^(0.5))
+			// = 1 / 0.5 
+			// = 2
+			return 2;
+			break;
+		case 2:
+			// pi / gamma(2)
+			// = pi / 1!
+			// = pi
+			return constantPi<Real>();
+			break;
+		case 3:
+			// pi^(1.5) / gamma(2.5)
+			// = pi^(1.5) / (1.5 gamma(1.5))
+			// = pi^(1.5) / (1.5 * 0.5 * gamma(0.5))
+			// = pi^(1.5) / (1.5 * 0.5 * pi^(0.5))
+			// = pi / ((3 / 2) * (1 / 2))
+			// = pi / (3 / 4)
+			// = (4 / 3) pi
+			return ((Real)4 / 3) * constantPi<Real>();
+			break;
+		};
+
+		Real nHalf = (Real)n / 2;
+		return std::pow(constantPi<Real>(), nHalf) / gamma<Real>(nHalf + 1);
+	} 
 
 	template <typename Real>
 	Real lnVolumeUnitSphereMinkowski(
@@ -50,69 +131,34 @@ namespace Pastel
 	{
 		PENSURE_OP(n, >, 0);
 
-		// S = {x in R^n : max(|x_1|, ..., |x_n|) <= 1}
-		//   = [-1, 1]^n
-		// => 
-		// volume(S) = 2^n
-		// =>
-		// ln(volume(S)) = n log(2)
+		// See volumeUnitSphereInfinity() for the
+		// derivation of the volume.
+
+		// ln(volume(S)) 
+		// = ln(2^n)
+		// = n ln(2)
 
 		return n * constantLn2<Real>();
 	}
 
-	template <typename Real, int N>
-	PASTEL_ENABLE_IF_C(N == 1, Real)
-		volumeUnitSphere()
-	{
-		return 2;
-	}
-
-	template <typename Real, int N>
-	PASTEL_ENABLE_IF_C(N == 2, Real)
-		volumeUnitSphere()
-	{
-		return constantPi<Real>();
-	}
-
-	template <typename Real, int N>
-	PASTEL_ENABLE_IF_C(N == 3, Real)
-		volumeUnitSphere()
-	{
-		return ((real)4 / 3) * constantPi<Real>();
-	}
-
-	template <typename Real, int N>
-	PASTEL_ENABLE_IF_C(N > 3, Real)
-		volumeUnitSphere()
-	{
-		return areaUnitSphere<Real, N>() / N;
-	}
-
 	template <typename Real>
-	Real volume(const Sphere<Real, 1>& sphere)
+	Real volumeUnitSphereInfinity(integer n)
 	{
-		return volumeUnitSphere<Real, 1>() * 
-			sphere.radius();
-	}
+		PENSURE_OP(n, >, 0);
 
-	template <typename Real>
-	Real volume(const Sphere<Real, 2>& sphere)
-	{
-		return volumeUnitSphere<Real, 2>() * 
-			square(sphere.radius());
-	}
+		// S = {x in R^n : max(|x_1|, ..., |x_n|) <= 1}
+		//   = [-1, 1]^n
+		// => 
+		// volume(S) = 2^n
 
-	template <typename Real>
-	Real volume(const Sphere<Real, 3>& sphere)
-	{
-		return volumeUnitSphere<Real, 3>() * 
-			square(sphere.radius()) * sphere.radius();
+		return std::pow((Real)2, (Real)n);
 	}
 
 	template <typename Real, int N>
 	Real volume(const Sphere<Real, N>& sphere)
 	{
-		return volumeUnitSphere<Real, N>() * std::pow(sphere.radius(), (Real)N);
+		return volumeUnitSphere<Real>(sphere.n()) * 
+			std::pow(sphere.radius(), sphere.n());
 	}
 
 }
