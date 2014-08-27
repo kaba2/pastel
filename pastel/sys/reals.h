ab1982443c59a6067d5eeab87603de467dfb4e40
Kalle Rutanen <kaba@hilvi.org>
2014-08-27 16:17:55 +0300

    Towards header-only...

--- pastel/sys/reals.h
+++ pastel/sys/reals.h
@@ -6,7 +6,7 @@
 #include "pastel/sys/real_concept.h"
 
 #include "pastel/sys/native_real.h"
-#include "pastel/sys/rational_tools.h"
-#include "pastel/sys/statreal_tools.h"
+#include "pastel/sys/rational.h"
+#include "pastel/sys/statreal.h"
 
 #endif
