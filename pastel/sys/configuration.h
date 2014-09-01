d2de5ee1daa489cf57541fad6ecde53831d75cc4
kaba <kaba@localhost>
2014-09-01 20:50:09 +0300

    Removed config files.

--- pastel/sys/configuration.h
+++
@@ -1,9 +0,0 @@
-// Description: An aggregate file for configuration
-
-#ifndef PASTELSYS_CONFIGURATION_H
-#define PASTELSYS_CONFIGURATION_H
-
-#include "pastel/sys/config_tools.h"
-#include "pastel/sys/configfile.h"
-
-#endif
