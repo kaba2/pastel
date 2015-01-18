5615283593970ec0579e5f44d2c77c52a6cc4004
kaba <kaba@localhost>
2015-01-18 14:05:46 +0200

    Added some module files.

---
+++ pastel/gfx/quantization.h
@@ -0,0 +1,9 @@
+// Description: Quantization module
+// Documentation: image_quantization.txt
+
+#ifndef PASTELGFX_QUANTIZATION_H_MODULE
+#define PASTELGFX_QUANTIZATION_H_MODULE
+
+#include "pastel/gfx/quantization/image_quantization.h"
+
+#endif
