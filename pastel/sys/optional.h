562badf4339fc2bb95702783d29681cc396645d7
Kalle Rutanen <kaba@hilvi.org>
2015-01-19 20:27:28 +0200

    Added module file.

---
+++ pastel/sys/optional.h
@@ -0,0 +1,9 @@
+// Description: Optional module
+// Documentation: optional.txt
+
+#ifndef PASTELSYS_OPTIONAL_H_MODULE
+#define PASTELSYS_OPTIONAL_H_MODULE
+
+#include "pastel/sys/optional/optional/optional.h"
+
+#endif
