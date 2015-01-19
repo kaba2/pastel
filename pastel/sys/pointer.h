8459d748068dd124a24732e250d30c0737c0cc3a
Kalle Rutanen <kaba@hilvi.org>
2015-01-19 20:12:00 +0200

    Added module files.

---
+++ pastel/sys/pointer.h
@@ -0,0 +1,12 @@
+// Description: Pointer module
+
+#ifndef PASTELSYS_POINTER_H_MODULE
+#define PASTELSYS_POINTER_H_MODULE
+
+#include "pastel/sys/pointer/arrayptr.h"
+#include "pastel/sys/pointer/clone_ptr.h"
+#include "pastel/sys/pointer/countedptr.h"
+#include "pastel/sys/pointer/weakptr.h"
+
+#endif
+
