fb21d36b6bd66cbac36d521b3f3bac6a1e970395
kaba <kaba@localhost>
2015-01-19 20:12:00 +0200

    Added module files.

---
+++ pastel/sys/allocator.h
@@ -0,0 +1,9 @@
+// Description: Allocator module
+// Documentation: allocators.txt
+
+#ifndef PASTELSYS_ALLOCATOR_H_MODULE
+#define PASTELSYS_ALLOCATOR_H_MODULE
+
+#include "pastel/sys/allocator/allocators.h"
+
+#endif
--- pastel/sys/memory.h
+++ pastel/sys/memory.h
@@ -3,8 +3,9 @@
 #ifndef PASTELSYS_MEMORY_H
 #define PASTELSYS_MEMORY_H
 
-#include "pastel/sys/allocators.h"
+#include "pastel/sys/allocator.h"
 #include "pastel/sys/generic/addressof.h"
 #include "pastel/sys/memory_overlaps.h"
+#include "pastel/sys/pointer.h"
 
 #endif
