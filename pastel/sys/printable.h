eda69259b367cb471c211877730b311d42fd4115
Kalle Rutanen <kaba@hilvi.org>
2015-03-27 22:20:33 +0200

    Added the printable concept.

---
+++ pastel/sys/printable.h
@@ -0,0 +1,9 @@
+// Description: Printable module
+
+#ifndef PASTELSYS_PRINTABLE_H_MODULE
+#define PASTELSYS_PRINTABLE_H_MODULE
+
+#include "pastel/sys/printable/printable_concept.h"
+#include "pastel/sys/printable/native_printable.h"
+
+#endif
