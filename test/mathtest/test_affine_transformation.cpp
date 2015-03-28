8763f55ff795c0be61f080c91a5622a0273d40cb
kaba <kaba@localhost>
2015-03-28 02:14:19 +0200

    Fixing radial texture.

--- test/mathtest/test_affine_transformation.cpp
+++ test/mathtest/test_affine_transformation.cpp
@@ -45,6 +45,9 @@ namespace
 				TEST_ENSURE(a.matrix() == identityMatrix<real>(3, 3));
 				TEST_ENSURE(allEqual(a.translation(), 0));
 				TEST_ENSURE_OP(a.n(), ==, 3);
+
+				TEST_ENSURE(transformPoint(a, Vector3(0, 0, 0)) == Vector3(0, 0, 0));
+				TEST_ENSURE(transformVector(a, Vector3(1, 1, 1)) == Vector3(1, 1, 1));
 			}
 			
 			// Constructs from a matrix expression.
@@ -53,6 +56,9 @@ namespace
 				TEST_ENSURE(b.matrix() == diagonalMatrix(Vector3(1, 2, 3)));
 				TEST_ENSURE(allEqual(b.translation(), 0));
 				TEST_ENSURE_OP(b.n(), ==, 3);
+
+				TEST_ENSURE(transformPoint(b, Vector3(1, 1, 1)) == Vector3(1, 2, 3));
+				TEST_ENSURE(transformVector(b, Vector3(1, 1, 1)) == Vector3(1, 2, 3));
 			}
 
 			// Constructs from a matrix and a vector expression.
@@ -61,6 +67,9 @@ namespace
 				TEST_ENSURE(c.translation() == Vector3(-1, 0, 1));
 				TEST_ENSURE(c.matrix() == diagonalMatrix(Vector3(1, 2, 3)));
 				TEST_ENSURE_OP(c.n(), ==, 3);
+
+				TEST_ENSURE(transformPoint(c, Vector3(1, 1, 1)) == Vector3(1 * 1 - 1, 1 * 2 - 0, 1 * 3 + 1));
+				TEST_ENSURE(transformVector(c, Vector3(1, 1, 1)) == Vector3(1 * 1, 1 * 2, 1 * 3));
 			}
 		}
 
