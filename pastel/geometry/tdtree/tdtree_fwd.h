2f5d9d6e93c1f6f2f3c1900e4e53c78f853aa362
kaba <kaba@localhost>
2015-04-13 16:34:15 +0300

    Updated documentation for TdTree.

--- pastel/geometry/tdtree/tdtree_fwd.h
+++ pastel/geometry/tdtree/tdtree_fwd.h
@@ -23,6 +23,9 @@ namespace Pastel
 		PASTEL_FWD(Locator);
 
 		//! The type of the contained points.
+		/*!
+		We denote the set of points by P.
+		*/
 		using Point = typename Locator::Point;
 
 		//! The type of the point-coordinates.
@@ -33,9 +36,9 @@ namespace Pastel
 
 		//! Storage for the temporal points.
 		/*!
-		The temporal points are stored in an array at each node.
-		The storage is an array, because we need indexing for the
-		fractional cascading.
+		The temporal points are stored in an array. The storage is an 
+		array, because we need indexing for the fractional cascading.
+		The entries in each node refer to this point-array by indices.
 		*/
 		using PointSet = 
 			std::vector<Temporal_Point<Point, Real>>;
@@ -48,10 +51,37 @@ namespace Pastel
 		using ConstIterator = Point_ConstIterator;
 
 		//! A node.
+		/*!
+		Each node Q is associated with 
+		* a closed rectangular subset C(Q) of R^d,
+		* a node L(Q), the left child-node, such that C(L(Q))) subset C(Q),
+		* a node R(Q), the right child-node, such that C(R(Q)) subset C(Q),
+		* a set of points P(Q), such that P(Q) = intersect {P(Q), P, C(Q)}.
+
+		It holds that [C(L(Q)) union C(R(Q))] = C(Q).
+		*/
 		class Node;
+
+		//! An entry.
+		/*!
+		An entry is a pairing of a point-iterator and two fractional
+		cascading indices; one index for each child-node.
+		*/
 		class Entry;
+
+		//! A cursor.
+		/*!
+		A cursor is used to identify a node, and to move between nodes.
+		*/
 		class Cursor;
 
+		//! Storage for the entry-set of a node.
+		/*!
+		Each node stores, in an array, a set of entries. 
+		Given an index-interval [i_0, i_1), the 
+		index-interval for the left child-node is given by
+		[L(i_0), L(i_1)). Similarly for the right child-node.
+		*/
 		using EntrySet = std::vector<Entry>;
 		using Entry_Iterator = typename EntrySet::iterator;
 		using Entry_ConstIterator = typename EntrySet::const_iterator;
