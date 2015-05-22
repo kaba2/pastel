9c5f2bf504edffc0a22278145668cafba7dc3ae1
kaba <kaba@localhost>
2015-05-22 11:50:14 +0300

    Worked on native integers.

--- test/systest/test_native_integers.cpp
+++ test/systest/test_native_integers.cpp
@@ -21,8 +21,49 @@ namespace
 		virtual void run()
 		{
 			testInfinity();
-			testEvenOdd();
-			testIsPowerOfTwo();
+
+			//testMany<bool>();
+			testMany<int8>();
+			testMany<int16>();
+			testMany<int32>();
+			testMany<int64>();
+
+			testMany<uint8>();
+			testMany<uint16>();
+			testMany<uint32>();
+			testMany<uint64>();
+
+			testMany<schar>();
+			testMany<char>();
+			testMany<short>();
+			testMany<int>();
+			testMany<long>();
+			testMany<longlong>();
+
+			testMany<uchar>();
+			testMany<ushort>();
+			testMany<uint>();
+			testMany<ulong>();
+			testMany<ulonglong>();
+		}
+
+		template <typename Type>
+		void testMany()
+		{
+			PASTEL_CONCEPT_CHECK(Type, Finite_Integer_Concept);
+
+			testComparison<Type>();
+			testEvenOdd<Type>();
+			testIsPowerOfTwo<Type>();
+		}
+
+		template <typename Type>
+		void testComparison()
+		{
+			TEST_ENSURE(negative((Type)-1) || std::is_unsigned<Type>::value);
+			TEST_ENSURE(!negative((Type)0));
+			TEST_ENSURE(positive((Type)1));
+			TEST_ENSURE(!positive((Type)0));
 		}
 
 		void testInfinity()
@@ -38,79 +79,62 @@ namespace
 			TEST_ENSURE(infinity<int64>() == 0x7FFFFFFFFFFFFFFFull);
 		}
 
+		template <typename Type>
 		void testEvenOdd()
 		{
 			for (integer i = 1;i < 100;i += 2)
 			{
-				TEST_ENSURE(odd((char)i));
-				TEST_ENSURE(odd((short)i));
-				TEST_ENSURE(odd((int)i));
-				TEST_ENSURE(odd((long)i));
-				TEST_ENSURE(odd((longlong)i));
-
-				TEST_ENSURE(odd((uchar)i));
-				TEST_ENSURE(odd((ushort)i));
-				TEST_ENSURE(odd((uint)i));
-				TEST_ENSURE(odd((ulong)i));
-				TEST_ENSURE(odd((ulonglong)i));
+				TEST_ENSURE(odd((Type)i));
 			}
 
 			for (integer i = 0;i < 100;i += 2)
 			{
-				TEST_ENSURE(even((char)i));
-				TEST_ENSURE(even((short)i));
-				TEST_ENSURE(even((int)i));
-				TEST_ENSURE(even((long)i));
-				TEST_ENSURE(even((longlong)i));
-				
-				TEST_ENSURE(even((uchar)i));
-				TEST_ENSURE(even((ushort)i));
-				TEST_ENSURE(even((uint)i));
-				TEST_ENSURE(even((ulong)i));
-				TEST_ENSURE(even((ulonglong)i));
+				TEST_ENSURE(even((Type)i));
 			}
 		}
 
+		template <typename Type>
 		void testIsPowerOfTwo()
 		{
-			TEST_ENSURE(isPowerOfTwo(1));
-			TEST_ENSURE(isPowerOfTwo(2));
-			TEST_ENSURE(!isPowerOfTwo(3));
-			TEST_ENSURE(isPowerOfTwo(4));
-			TEST_ENSURE(!isPowerOfTwo(5));
-			TEST_ENSURE(!isPowerOfTwo(6));
-			TEST_ENSURE(!isPowerOfTwo(7));
-			TEST_ENSURE(isPowerOfTwo(8));
-			TEST_ENSURE(!isPowerOfTwo(9));
-			TEST_ENSURE(!isPowerOfTwo(10));
-			TEST_ENSURE(!isPowerOfTwo(11));
-			TEST_ENSURE(!isPowerOfTwo(12));
-			TEST_ENSURE(!isPowerOfTwo(13));
-			TEST_ENSURE(!isPowerOfTwo(14));
-			TEST_ENSURE(!isPowerOfTwo(15));
-			TEST_ENSURE(isPowerOfTwo(16));
-			TEST_ENSURE(!isPowerOfTwo(17));
-			TEST_ENSURE(!isPowerOfTwo(18));
-			TEST_ENSURE(!isPowerOfTwo(19));
-			TEST_ENSURE(!isPowerOfTwo(20));
-			TEST_ENSURE(!isPowerOfTwo(21));
-			TEST_ENSURE(!isPowerOfTwo(22));
-			TEST_ENSURE(!isPowerOfTwo(23));
-			TEST_ENSURE(!isPowerOfTwo(24));
-			TEST_ENSURE(!isPowerOfTwo(25));
-			TEST_ENSURE(!isPowerOfTwo(26));
-			TEST_ENSURE(!isPowerOfTwo(27));
-			TEST_ENSURE(!isPowerOfTwo(28));
-			TEST_ENSURE(!isPowerOfTwo(29));
-			TEST_ENSURE(!isPowerOfTwo(30));
-			TEST_ENSURE(!isPowerOfTwo(31));
-			TEST_ENSURE(isPowerOfTwo(32));
-			TEST_ENSURE(isPowerOfTwo(64));
-			TEST_ENSURE(isPowerOfTwo(128));
-			TEST_ENSURE(isPowerOfTwo(256));
-			TEST_ENSURE(isPowerOfTwo(512));
-			TEST_ENSURE(!isPowerOfTwo(511));
-			TEST_ENSURE(!isPowerOfTwo(513));
+			auto f = [](integer t)
+			{
+				return isPowerOfTwo((Type)t);
+			};
+
+			TEST_ENSURE(f(1));
+			TEST_ENSURE(f(2));
+			TEST_ENSURE(!f(3));
+			TEST_ENSURE(f(4));
+			TEST_ENSURE(!f(5));
+			TEST_ENSURE(!f(6));
+			TEST_ENSURE(!f(7));
+			TEST_ENSURE(f(8));
+			TEST_ENSURE(!f(9));
+			TEST_ENSURE(!f(10));
+			TEST_ENSURE(!f(11));
+			TEST_ENSURE(!f(12));
+			TEST_ENSURE(!f(13));
+			TEST_ENSURE(!f(14));
+			TEST_ENSURE(!f(15));
+			TEST_ENSURE(f(16));
+			TEST_ENSURE(!f(17));
+			TEST_ENSURE(!f(18));
+			TEST_ENSURE(!f(19));
+			TEST_ENSURE(!f(20));
+			TEST_ENSURE(!f(21));
+			TEST_ENSURE(!f(22));
+			TEST_ENSURE(!f(23));
+			TEST_ENSURE(!f(24));
+			TEST_ENSURE(!f(25));
+			TEST_ENSURE(!f(26));
+			TEST_ENSURE(!f(27));
+			TEST_ENSURE(!f(28));
+			TEST_ENSURE(!f(29));
+			TEST_ENSURE(!f(30));
+			TEST_ENSURE(!f(31));
+			TEST_ENSURE(f(32));
+			TEST_ENSURE(f(64));
+			TEST_ENSURE(!f(127));
 		}
 	};
 
