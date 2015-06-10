ef2bea1bebcc526941fd5784b201462c6b7f0620
kaba <kaba@localhost>
2015-06-10 22:42:14 +0300

    Added picking random element from an input.

--- pastel/sys/random/random_discrete.h
+++ pastel/sys/random/random_discrete.h
@@ -3,6 +3,7 @@
 #ifndef PASTELSYS_RANDOM_DISCRETE_H
 #define PASTELSYS_RANDOM_DISCRETE_H
 
+#include "pastel/sys/random/random_element.h"
 #include "pastel/sys/random/random_integer.h"
 #include "pastel/sys/random/random_geometric.h"
 
---
+++ pastel/sys/random/random_element.h
@@ -0,0 +1,38 @@
+// Description: Random element in a range
+
+#ifndef PASTELSYS_RANDOM_ELEMENT_H
+#define PASTELSYS_RANDOM_ELEMENT_H
+
+#include "pastel/sys/input/indexed_input_concept.h"
+#include "pastel/sys/input/range_input.h"
+#include "pastel/sys/random/random_integer.h"
+
+#include <initializer_list>
+
+namespace Pastel
+{
+
+
+	template <
+		typename Indexed_Input,
+		Requires<
+			Models<Indexed_Input, Indexed_Input_Concept>
+		> = 0>
+	decltype(auto) randomElement(
+		Indexed_Input input)
+	{
+		PENSURE_OP(input.n(), >, 0);
+		return input[randomInteger(input.n())];
+	}
+
+	template <typename Type>
+	decltype(auto) randomElement(
+		const std::initializer_list<Type>& elementSet)
+	{
+		using Pastel::randomElement;
+		return randomElement(rangeInput(elementSet));
+	}
+
+}
+
+#endif
