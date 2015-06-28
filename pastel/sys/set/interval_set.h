310d94a2c07013986f918ce19710bfee170459af
Kalle Rutanen <kaba@hilvi.org>
2015-06-28 23:15:44 +0300

    Added missing set.

---
+++ pastel/sys/set/interval_set.h
@@ -0,0 +1,92 @@
+// Description: Interval multi-set
+
+#ifndef PASTELSYS_INTERVAL_MULTISET_H
+#define PASTELSYS_INTERVAL_MULTISET_H
+
+#include "pastel/sys/set/set_concept.h"
+#include "pastel/sys/algebra/element_concept.h"
+
+namespace Pastel
+{
+
+	//! An interval element.
+	struct Interval_Element_Concept
+		: Refines<Element_Concept>
+	{
+		template <typename Type>
+		auto requires(Type&& t) -> decltype
+		(
+			conceptCheck(
+				//! Constructs from another element.
+				Type{t},
+				//! Advances to the next element.
+				++t,
+				//! Computes the distance between elements.
+				Concept::convertsTo<integer>(t - t)
+			)
+		);
+	};
+
+}
+
+namespace Pastel
+{
+
+	template <
+		typename Element_,
+		Requires<
+			Models<Element_, Interval_Element_Concept>
+		> = 0
+	>
+	class Interval_Set
+	{
+	public:
+		using Element = Element_;
+
+		Interval_Set(
+			Element begin,
+			Element end)
+		: begin_(begin)
+		, end_(end)
+		{
+		}
+
+		integer n() const
+		{
+			return end_ - begin_;
+		}
+
+		template <typename Visit>
+		bool forEach(const Visit& visit) const
+		{
+			for (Element i{begin_};i != end_;++i)
+			{
+				if (!visit(i))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+	private:
+		Element begin_;
+		Element end_;
+	};
+
+	template <typename Element>
+	Interval_Set<RemoveCvRef<Element>> intervalSet(
+		Element&& begin, 
+		Element&& end)
+	{
+		return 
+		{
+			std::forward<Element>(begin), 
+			std::forward<Element>(end)
+		};
+	}
+
+}
+
+#endif
