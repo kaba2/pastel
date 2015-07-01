3e346acb0a458ae808ff6baa086011b823522a69
Kalle Rutanen <kaba@hilvi.org>
2015-07-01 17:17:31 +0300

    Updated function tests.

--- test/systest/test_function.cpp
+++ test/systest/test_function.cpp
@@ -27,14 +27,27 @@ namespace
 		void test()
 		{
 			using Pair = std::pair<integer, real>;
-			Pair a;
-
-			PASTEL_STATIC_ASSERT(std::is_same<decltype(First_Function()(a)), integer&>::value);
+			PASTEL_STATIC_ASSERT(std::is_same<decltype(First_Function()(Pair())), integer&>::value);
+			PASTEL_STATIC_ASSERT(!std::is_same<decltype(First_Function()(Pair())), real&>::value);
+			PASTEL_STATIC_ASSERT(std::is_same<decltype(Second_Function()(Pair())), real&>::value);
+			PASTEL_STATIC_ASSERT(!std::is_same<decltype(Second_Function()(Pair())), integer&>::value);
 
 			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer, Pair));
 			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer&, Pair));
-			PASTEL_CONCEPT_REJECT(First_Function, Function_Concept(real&, Pair));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const integer&, Pair));
+			
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer, Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer&, Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const integer&, Pair&));
 
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer, const Pair&));
+			PASTEL_CONCEPT_REJECT(First_Function, Function_Concept(integer&, const Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const integer&, const Pair&));
+
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(real, Pair));
+			PASTEL_CONCEPT_REJECT(First_Function, Function_Concept(real&, Pair));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const real&, Pair));
+			
 			PASTEL_CONCEPT_CHECK(Second_Function, Function_Concept(real, Pair));
 			PASTEL_CONCEPT_CHECK(Second_Function, Function_Concept(real&, Pair));
 			PASTEL_CONCEPT_REJECT(Second_Function, Function_Concept(integer&, Pair));
