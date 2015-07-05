5912b514e5372568b4fc1a9d1a23ee0cee321c91
kaba <kaba@localhost>
2015-07-05 15:50:00 +0300

    Added missing PointSet_Point.

---
+++ pastel/sys/pointset/pointset_point.h
@@ -0,0 +1,50 @@
+// Description: Point-type of a point-set
+// DocumentationOf: pointset_concept.h
+
+#ifndef PASTELSYS_POINTSET_POINT_H
+#define PASTELSYS_POINTSET_POINT_H
+
+#include "pastel/sys/pointset/pointset_concept.h"
+#include "pastel/sys/pointset/pointset_point_id.h"
+#include "pastel/sys/set/set_concept.h"
+#include "pastel/sys/locator/location_set.h"
+#include "pastel/sys/function/identity_function.h"
+#include "pastel/sys/type_traits/remove_cvref.h"
+
+#include <type_traits>
+
+namespace Pastel
+{
+
+	namespace PointSet_Point_
+	{
+
+		template <typename PointSet>
+		struct PointSet_Point_F_
+		{
+			using type = PointSet_PointId<PointSet>;
+		};
+
+		template <
+			typename Set, 
+			typename Locator>
+		struct PointSet_Point_F_<LocationSet<Set, Locator>>
+		{
+			using type = Location<Set_Element<Set>, Locator>;
+		};
+
+	}
+
+	template <typename PointSet>
+	using PointSet_Point = 
+		typename PointSet_Point_::PointSet_Point_F_<
+			RemoveCvRef<PointSet>
+		>::type;
+
+	template <typename PointSet>
+	using PointSet_Point_F = 
+		Identity_F<PointSet_Point<PointSet>>;
+
+}
+
+#endif
