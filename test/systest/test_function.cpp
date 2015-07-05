c08123dd009f325fc6a80f550e8f3d868e3c8314
Kalle Rutanen <kaba@hilvi.org>
2015-07-05 14:36:00 +0300

    Made Function_Concept work with void return-types.

--- test/systest/test_function.cpp
+++ test/systest/test_function.cpp
@@ -21,10 +21,22 @@ namespace
 
 		virtual void run()
 		{
-			test();
+			testFirstAndSecond();
 		}
 
-		void test()
+		void testArchetype()
+		{
+			{
+				using F = Function_Archetype<void, integer>;
+				PASTEL_CONCEPT_CHECK(F, Function_Concept<void>(integer));
+			}
+			{
+				using F = Function_Archetype<integer, integer>;
+				PASTEL_CONCEPT_CHECK(F, Function_Concept<integer>(integer));
+			}
+		}
+
+		void testFirstAndSecond()
 		{
 			using Pair = std::pair<integer, real>;
 			PASTEL_STATIC_ASSERT(std::is_same<decltype(First_Function()(Pair())), integer&>::value);
@@ -32,25 +44,25 @@ namespace
 			PASTEL_STATIC_ASSERT(std::is_same<decltype(Second_Function()(Pair())), real&>::value);
 			PASTEL_STATIC_ASSERT(!std::is_same<decltype(Second_Function()(Pair())), integer&>::value);
 
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer, Pair));
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer&, Pair));
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const integer&, Pair));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<integer>(Pair));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<integer&>(Pair));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<const integer&>(Pair));
 			
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer, Pair&));
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer&, Pair&));
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const integer&, Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<integer>(Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<integer&>(Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<const integer&>(Pair&));
 
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(integer, const Pair&));
-			PASTEL_CONCEPT_REJECT(First_Function, Function_Concept(integer&, const Pair&));
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const integer&, const Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<integer>(const Pair&));
+			PASTEL_CONCEPT_REJECT(First_Function, Function_Concept<integer&>(const Pair&));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<const integer&>(const Pair&));
 
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(real, Pair));
-			PASTEL_CONCEPT_REJECT(First_Function, Function_Concept(real&, Pair));
-			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept(const real&, Pair));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<real>(Pair));
+			PASTEL_CONCEPT_REJECT(First_Function, Function_Concept<real&>(Pair));
+			PASTEL_CONCEPT_CHECK(First_Function, Function_Concept<const real&>(Pair));
 			
-			PASTEL_CONCEPT_CHECK(Second_Function, Function_Concept(real, Pair));
-			PASTEL_CONCEPT_CHECK(Second_Function, Function_Concept(real&, Pair));
-			PASTEL_CONCEPT_REJECT(Second_Function, Function_Concept(integer&, Pair));
+			PASTEL_CONCEPT_CHECK(Second_Function, Function_Concept<real>(Pair));
+			PASTEL_CONCEPT_CHECK(Second_Function, Function_Concept<real&>(Pair));
+			PASTEL_CONCEPT_REJECT(Second_Function, Function_Concept<integer&>(Pair));
 		}
 	};
 
