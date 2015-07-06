670a820de4ac4eec69f948793725e161d57f54ff
Kalle Rutanen <kaba@hilvi.org>
2015-07-06 13:23:22 +0300

    Fixed include-guard name.

--- pastel/sys/graph/transitive_closure.h
+++ pastel/sys/graph/transitive_closure.h
@@ -1,8 +1,8 @@
 // Description: Transitive closure module
 // Documentation: transitive_closure.txt
 
-#ifndef PASTELSYS_GRAPH_TRANSITIVE_CLOSURE_H_MODULE
-#define PASTELSYS_GRAPH_TRANSITIVE_CLOSURE_H_MODULE
+#ifndef PASTELSYS_TRANSITIVE_CLOSURE_H_MODULE
+#define PASTELSYS_TRANSITIVE_CLOSURE_H_MODULE
 
 #include "pastel/sys/graph/transitive_closure/transitive_closure.h"
 
