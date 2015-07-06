3f351d26ad89125e4c313e4ceadaf5c748435161
Kalle Rutanen <kaba@hilvi.org>
2015-07-06 23:59:28 +0300

    Added Catch test library.

---
+++ test/test_init.h
@@ -0,0 +1,14 @@
+// Description: Initialization for tests.
+
+#ifndef PASTELTEST_INIT_H
+#define PASTELTEST_INIT_H
+
+#include "test/catch.hpp"
+
+namespace Pastel
+{
+}
+
+using namespace Pastel;
+
+#endif
