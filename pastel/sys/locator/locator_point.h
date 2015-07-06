7ffa3d353237c11f0e522772cfc97db9294ebf1c
kaba <kaba@localhost>
2015-07-06 13:23:03 +0300

    Added more concept-checking to Locator.

--- pastel/sys/locator/locator_point.h
+++ pastel/sys/locator/locator_point.h
@@ -4,17 +4,27 @@
 #define PASTELSYS_LOCATOR_POINT_H
 
 #include "pastel/sys/locator/locator_concept.h"
-
-#include <type_traits>
+#include "pastel/sys/function/identity_function.h"
+#include "pastel/sys/type_traits/remove_cvref.h"
 
 namespace Pastel
 {
 
-	template <typename Locator>
+	template <
+		typename Locator,
+		Requires<
+			Models<Locator, Locator_Concept>
+		> = 0
+	>
 	using Locator_Point = 
-		typename std::remove_reference_t<Locator>::Point;
+		typename RemoveCvRef<Locator>::Point;
 
-	template <typename Locator>
+	template <
+		typename Locator,
+		Requires<
+			Models<Locator, Locator_Concept>
+		> = 0
+	>
 	using Locator_Point_F = 
 		Identity_F<Locator_Point<Locator>>;
 
