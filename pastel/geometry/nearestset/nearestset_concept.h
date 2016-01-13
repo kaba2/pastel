d3d8a6de9bd80ecd2fb0ac73ed32af8d59b5240c
kaba <kaba@localhost>
2016-01-13 21:09:46 +0200

    Made NearestSet concept check in full.

--- pastel/geometry/nearestset/nearestset_concept.h
+++ pastel/geometry/nearestset/nearestset_concept.h
@@ -18,6 +18,7 @@ namespace Pastel
 		template <
 			typename Type,
 			typename PointSet = NearestSet_PointSet<Type>,
+			typename PointId = PointSet_PointId<PointSet>,
 			typename Point = PointSet_Point<PointSet>,
 			typename Real = PointSet_Real<PointSet>
 		>
@@ -26,33 +27,30 @@ namespace Pastel
 			conceptCheck(
 				//! Returns the underlying point-set.
 				Concept::isModelOf<PointSet_Concept>(
-					addConst(t).pointSet())//,
+					addConst(t).pointSet()),
 				//! Searches for nearest neighbors for a point.
-				//Concept::convertsTo<std::pair<Real, Point>>
-				//(
-				#if 0
-				searchNearest(
-						// A nearest-set.
-						addConst(t),
-						//! The point for which to search nearest-neighbors.
-						std::declval<Point>()//,
-						//! The output to which to report the results.
-						/*!
-						The points will be reported as report(distance2, point).
-						*/
-						/*
-						PASTEL_TAG(report), Function_Archetype<void, Real, Point>(),
-						//! An indicator to decide whether to consider a given point.
-						PASTEL_TAG(accept), Function_Archetype<bool, Point>(),
-						//! The norm to use.
-						PASTEL_TAG(normBijection), NormBijection_Archetype(),
-						//! The number of nearest-neigbhors to search for.
-						PASTEL_TAG(kNearest), (integer)1,
-						//! The maximum distance to allow for a nearest-neighbors.
-						PASTEL_TAG(maxDistance2), (Real)Infinity()*/
-				//	)
+				Concept::convertsTo<std::pair<Real, PointId>>
+				(
+					searchNearest(
+							// A nearest-set.
+							addConst(t),
+							//! The point for which to search nearest-neighbors.
+							std::declval<Point>(),
+							//! The output to which to report the results.
+							/*!
+							The points will be reported as report(distance2, point).
+							*/
+							PASTEL_TAG(report), Function_Archetype<void, Real, PointId>(),
+							//! An indicator to decide whether to consider a given point.
+							PASTEL_TAG(accept), Function_Archetype<bool, PointId>(),
+							//! The norm to use.
+							PASTEL_TAG(normBijection), NormBijection_Archetype(),
+							//! The number of nearest-neigbhors to search for.
+							PASTEL_TAG(kNearest), (integer)1,
+							//! The maximum distance to allow for a nearest-neighbors.
+							PASTEL_TAG(maxDistance2), (Real)Infinity()
+					)
 				)
-				#endif
 			)
 		);
 	};
