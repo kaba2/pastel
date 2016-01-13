afe3d87964618f5687ff4e58f9cc98a22e491234
Kalle Rutanen <kaba@hilvi.org>
2016-01-13 21:54:36 +0200

    Updated documentation.

--- pastel/geometry/nearestset/nearestset_concept.h
+++ pastel/geometry/nearestset/nearestset_concept.h
@@ -32,23 +32,28 @@ namespace Pastel
 				Concept::convertsTo<std::pair<Real, PointId>>
 				(
 					searchNearest(
-							// A nearest-set.
-							addConst(t),
-							//! The point for which to search nearest-neighbors.
-							std::declval<Point>(),
-							//! The output to which to report the results.
-							/*!
-							The points will be reported as report(distance2, point).
-							*/
-							PASTEL_TAG(report), Function_Archetype<void, Real, PointId>(),
-							//! An indicator to decide whether to consider a given point.
-							PASTEL_TAG(accept), Function_Archetype<bool, PointId>(),
-							//! The norm to use.
-							PASTEL_TAG(normBijection), NormBijection_Archetype(),
-							//! The number of nearest-neigbhors to search for.
-							PASTEL_TAG(kNearest), (integer)1,
-							//! The maximum distance to allow for a nearest-neighbors.
-							PASTEL_TAG(maxDistance2), (Real)Infinity()
+						// A nearest-set.
+						addConst(t),
+						//! The point for which to search nearest-neighbors.
+						/*!
+						This should read Point_Archetype; the search-point does
+						not have to have Point type. However, for some reason that
+						triggers compiler errors.
+						*/
+						std::declval<Point>(),
+						//! The output to which to report the results.
+						/*!
+						The points will be reported as report(distance2, point).
+						*/
+						PASTEL_TAG(report), Function_Archetype<void, Real, PointId>(),
+						//! An indicator to decide whether to consider a given point.
+						PASTEL_TAG(accept), Function_Archetype<bool, PointId>(),
+						//! The norm to use.
+						PASTEL_TAG(normBijection), NormBijection_Archetype(),
+						//! The number of nearest-neigbhors to search for.
+						PASTEL_TAG(kNearest), (integer)1,
+						//! The maximum distance to allow for a nearest-neighbors.
+						PASTEL_TAG(maxDistance2), (Real)Infinity()
 					)
 				)
 			)
