b7d23c2d7fa10848ee0ff3c21f98daefd957a188
kaba2 <kaba2@localhost>
2017-03-15 00:33:50 +0200

    Updated tbb linux-path.

--- cmake/FindTbb_.cmake
+++ cmake/FindTbb_.cmake
@@ -40,7 +40,7 @@ elseif (APPLE)
 		"${TbbDirectory}/local/lib/lib${TbbLibraryName}.dylib")
 else()
 	set (TbbLibraryPath 
-		"${TbbDirectory}/lib/lib${TbbLibraryName}.so")
+		"${TbbDirectory}/libx86_64-linux-gnu/lib${TbbLibraryName}.so")
 endif()
 
 get_filename_component (TbbLibraryFilename "${TbbLibraryPath}" NAME)
