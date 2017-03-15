27e37a8e47d884e8d673ef1e555b1176b4ce9a6f
kaba2 <kaba2@localhost>
2017-03-15 22:10:15 +0200

    Added ranked set.

---
+++ pastel/sys/rankedset/rankedset.h
@@ -0,0 +1,144 @@
+// Documentation: Ranked set
+
+#ifndef PASTELSYS_RANKEDSET_H
+#define PASTELSYS_RANKEDSET_H
+
+#include "pastel/sys/mytypes.h"
+#include "pastel/sys/ensure.h"
+
+#include <vector>
+#include <algorithm>
+
+namespace Pastel
+{
+
+	//! A ranked set
+	/*!
+	Maintains the k smallest elements stored in the set;
+	a bounded priority queue. The k is called the capacity 
+	of the set.
+	*/
+	template <typename Type, typename Less = LessThan>
+	class RankedSet
+	{
+	public:
+		using DataSet = std::vector<Type>;
+		using Iterator = typename DataSet::iterator;
+		using ConstIterator = typename DataSet::const_iterator;
+		using iterator = Iterator;
+		using const_iterator = ConstIterator;
+
+		//! Constructs a set.
+		RankedSet(
+			integer capacity = 0,
+			Less less = Less())
+			: dataSet_()
+			, less_(std::move(less))
+		{
+			ENSURE_OP(capacity, >= , 0);
+			reserve(capacity);
+		}
+
+		//! Inserts an element into the set.
+		Iterator push(const Type& that)
+		{
+			if (capacity() == 0)
+			{
+				return end();
+			}
+
+			if (size() == capacity())
+			{
+				if (!less_(that, top()))
+				{
+					return end();
+				}
+
+				pop();
+			}
+			
+			dataSet_.push_back(that);
+			std::push_heap(begin(), end(), less_);
+			return end() - 1;
+		}
+
+		//! Removes the maximum element.
+		void pop()
+		{
+			ENSURE(!empty());
+			std::pop_heap(begin(), end(), less_);
+			dataSet_.pop_back();
+		}
+
+		//! Returns an iterator to the first element.
+		PASTEL_ITERATOR_FUNCTIONS(begin, dataSet_.begin());
+
+		//! Returns an iterator to the one-past-end element.
+		PASTEL_ITERATOR_FUNCTIONS(end, dataSet_.end());
+
+		//! Returns whether the set is empty.
+		bool empty() const
+		{
+			return dataSet_.empty();
+		}
+
+		//! Swaps two sets.
+		void swap(RankedSet& that)
+		{
+			using std::swap;
+
+			dataSet_.swap(that.dataSet_);
+			swap(less_, that.less_);
+		}
+
+		//! Returns the maximum element.
+		Type& top()
+		{
+			ENSURE(!empty());
+			return dataSet_.front();
+		}
+
+		//! Returns the maximum element.
+		const Type& top() const
+		{
+			ENSURE(!empty());
+			return dataSet_.front();
+		}
+
+		//! Returns the size of the set.
+		integer size() const
+		{
+			return dataSet_.size();
+		}
+
+		//! Sets the capacity of the set.
+		/*!
+		Preconditions:
+		capacity >= 0
+		*/
+		void reserve(integer capacity)
+		{
+			ENSURE_OP(capacity, >=, 0);
+
+			while(size() > capacity)
+			{
+				pop();
+			}
+
+			dataSet_.reserve(capacity);
+		}
+
+		//! Returns the capacity of the set.
+		integer capacity() const
+		{
+			return dataSet_.capacity();
+		}
+
+	private:
+		DataSet dataSet_;
+		Less less_;
+	};
+
+}
+
+#endif
