d639141842f7eafacf08ed3bd9420c3fdfbbb807
Kalle Rutanen <kaba@hilvi.org>
2017-03-18 11:31:05 +0200

    Removed old include.

--- pastel/sys/locator.h
+++ pastel/sys/locator.h
@@ -9,7 +9,6 @@
 #include "pastel/sys/locator/indirect_locator.h"
 #include "pastel/sys/locator/pointer_locator.h"
 #include "pastel/sys/locator/pointerrange_locator.h"
-#include "pastel/sys/locator/scalar_locator.h"
 #include "pastel/sys/locator/sub_locator.h"
 #include "pastel/sys/locator/transform_locator.h"
 
