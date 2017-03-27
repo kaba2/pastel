63b9c1a2136f18db4e6fdd5178b5657439a0d162
Kalle Rutanen <kaba@hilvi.org>
2017-03-27 20:57:18 +0300

    Refactored random matrices.

--- pastel/math/matrix/random_matrix/random_orthogonal_matrix.h
+++ pastel/math/matrix/random_matrix/random_orthogonal_matrix.h
@@ -4,6 +4,10 @@
 #define PASTELMATH_RANDOM_ORTHOGONAL_MATRIX_H
 
 #include "pastel/math/matrix/matrix.h"
+#include "pastel/sys/random/random_gaussian.h"
+#include "pastel/sys/math/sign.h"
+
+#include <armadillo>
 
 namespace Pastel
 {
@@ -26,11 +30,56 @@ namespace Pastel
 	If positive, then det(Q) = +1.
 	*/
 	template <typename Real>
-	Matrix<Real> randomOrthogonal(
-		integer n, integer orientation = 0);
+	arma::Mat<Real> randomOrthogonal(
+		integer n, integer orientation = 0)
+	{
+		ENSURE_OP(n, >=, 0);
+
+		// "How to Generate Random Matrices
+		// from the Classical Compact Groups",
+		// Francesco Mezzadri,
+		// Notices of the AMS,
+		// Volume 54, Number 5, 2007.
+
+		arma::Mat<Real> x(n, n);
+
+		for (auto& value : x)
+		{
+			value = randomGaussian<Real>();
+		}
+
+		arma::Mat<Real> q(n, n);
+		arma::Mat<Real> r(n, n);
+
+		qr(q, r, x);
+
+		for (integer i = 0;i < n;++i)
+		{
+			if (r(i, i)  < 0)
+			{
+				q.col(i) = -q.col(i);
+			}
+		}
+
+		if (orientation != 0)
+		{
+			if (sign(det(q)) != sign(orientation))
+			{
+				// While the following gives the correct
+				// determinant, and preserves orthogonality,
+				// I don't have a proof that this will preserve
+				// the uniform distribution property. This may
+				// or may not be correct.
+				for (integer i = 0;i < n;++i)
+				{
+					q(i, 0) = -q(i, 0);
+				}
+			}
+		}
+
+		return q;
+	}
 
 }
-
-#include "pastel/math/matrix/random_matrix/random_orthogonal_matrix.hpp"
 
 #endif
