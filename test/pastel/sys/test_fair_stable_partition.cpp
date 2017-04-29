40d87f7a2eba40135c5045d0cd634efb05b0a3b6
kaba2 <kaba2@localhost>
2017-04-29 19:02:42 +0300

    Made fair-stable-partition fairer.

--- test/pastel/sys/test_fair_stable_partition.cpp
+++ test/pastel/sys/test_fair_stable_partition.cpp
@@ -7,66 +7,99 @@
 #include <pastel/sys/trindicator.h>
 #include <pastel/sys/indicator.h>
 
+using vec = std::vector<int>;
+
 TEST_CASE("fair_stable_partition (fair_stable_partition)")
 {
+	auto test = [](
+		auto elementSet,
+		const auto& trindicator,
+		const auto& leftSet, 
+		const auto& rightSet)
 	{
-		std::vector<integer> elementSet =
-		{
-			1, 6, 3, 4, 6, 8, 9, 0, 4
-		};
-
-		auto trindicator = [](integer)
-		{
-			return 0;
-		};
-
 		auto rightBegin = 
 			fairStablePartition(
-				range(elementSet.begin(), elementSet.end()), trindicator);
-
-		std::vector<integer> leftSet =
+				range(elementSet.begin(), elementSet.end()), 
+				trindicator);
+		
+		if (!boost::equal(leftSet, 
+			range(elementSet.begin(), rightBegin)))
 		{
-			1, 3, 6, 9, 4
-		};
+			return false;
+		}
 
-		std::vector<integer> rightSet =
+		if (!boost::equal(rightSet, 
+			range(rightBegin, elementSet.end())))
 		{
-			6, 4, 8, 0
-		};
+			return false;
+		}
+		
+		return true;
+	};
 
-		REQUIRE(boost::equal(leftSet, 
-			range(elementSet.begin(), rightBegin)));
+	auto lessFive = [](auto element) {return element - 5;};
 
-		REQUIRE(boost::equal(rightSet, 
-			range(rightBegin, elementSet.end())));
-	}
+	REQUIRE(test(
+		vec({1, 6, 3, 4, 6, 8, 9, 0, 4}),
+		[](auto element) {return 0;},
+		vec({1, 3, 6, 9, 4}),
+		vec({6, 4, 8, 0})
+	));
 
-	{
-		std::vector<integer> elementSet =
-		{
-			1, 6, 3, 4, 6, 8, 9, 0, 4
-		};
+	REQUIRE(test(
+		vec({1, 6, 3, 4, 6, 8, 9, 0, 4}),
+		lessFive,
+		vec({1, 3, 4, 0, 4}),
+		vec({6, 6, 8, 9})
+	));
 
-		auto rightBegin =
-			fairStablePartition(
-			range(elementSet.begin(), elementSet.end()), 
-			indicatorTrindicator(predicateIndicator(5, LessThan())));
+	REQUIRE(test(
+		vec({1, 5}),
+		lessFive,
+		vec({1}),
+		vec({5})
+	));
 
-		std::vector<integer> leftSet =
-		{
-			1, 3, 4, 0, 4
-		};
+	REQUIRE(test(
+		vec({1, 2, 5}),
+		lessFive,
+		vec({1, 2}),
+		vec({5})
+	));
 
-		std::vector<integer> rightSet =
-		{
-			6, 6, 8, 9
-		};
+	REQUIRE(test(
+		vec({5, 6}),
+		lessFive,
+		vec({5}),
+		vec({6})
+	));
 
-		REQUIRE(boost::equal(leftSet,
-			range(elementSet.begin(), rightBegin)));
+	REQUIRE(test(
+		vec({5, 6, 7}),
+		lessFive,
+		vec({5}),
+		vec({6, 7})
+	));
 
-		REQUIRE(boost::equal(rightSet,
-			range(rightBegin, elementSet.end())));
-	}
+	REQUIRE(test(
+		vec({5, 5}),
+		lessFive,
+		vec({5}),
+		vec({5})
+	));
+
+	REQUIRE(test(
+		vec({5, 5, 5}),
+		lessFive,
+		vec({5, 5}),
+		vec({5})
+	));
+
+	REQUIRE(test(
+		vec({5, 5, 5, 5}),
+		lessFive,
+		vec({5, 5}),
+		vec({5, 5})
+	));
 }
 
