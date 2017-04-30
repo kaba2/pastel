fa76be9855c323c381c103def74933e5363724c1
Kalle Rutanen <kaba@hilvi.org>
2017-05-01 01:03:02 +0300

    Fixed fair-stable-partitioning.

--- pastel/sys/sequence/fair_stable_partition.hpp
+++ pastel/sys/sequence/fair_stable_partition.hpp
@@ -20,53 +20,76 @@ namespace Pastel
 			using Type = typename boost::range_value<Range>::type;
 
 			// Compute the number of elements.
-			integer n = boost::size(elementSet);
+			const integer n = boost::size(elementSet);
 			
+			integer zeros = 0;
+			integer negatives = 0;
+			integer positives = 0;
+
+			RANGES_FOR(auto&& element, elementSet)
+			{
+				const integer i = trindicator(element);
+
+				if (i > 0)
+				{
+					++positives;
+				}
+				else if (i < 0)
+				{
+					++negatives;
+				}
+				else
+				{
+					++zeros;
+				}
+			}
+
+			Iterator begin = std::begin(elementSet);
+			Iterator end = std::end(elementSet);
+
+			integer negativeZeros = (zeros + 1) / 2;
+			if (negatives > positives && (zeros & 1) == 1)
+			{
+				--negativeZeros;
+			}
+
+			const integer positiveZeros = zeros - negativeZeros;
+
 			// We will first move the elements with a
 			// positive indicator into this temporary set.
 			// At the end of the algorithm we will move 
 			// these elements to the end of the original 
 			// sequence.
 			std::vector<Type> positiveSet;
-			positiveSet.reserve(n);
-
-			integer zeros = 0;
-
-			// To make the partition 'fair', we will interpret
-			// every second element with a zero indicator
-			// as a negative element, and the rest as 
-			// positive elements. 
-			integer zeroIndicator = -1;
-
-			Iterator begin = std::begin(elementSet);
-			Iterator end = std::end(elementSet);
+			positiveSet.reserve(positives + positiveZeros);
 
 			// The end of the negative set. This will
 			// be tracked throughout the algorithm.
 			Iterator negativeEnd = begin;
-			
-			Iterator current = begin;
-			while (current != end)
+
+			// The number of zero-indicators seen
+			// thus far.
+			integer zerosThusfar = 0;
+			RANGES_FOR(auto&& element, elementSet)
 			{
 				// Find out the indicator of the
 				// current element.
-				integer i = trindicator(*current);
+				integer i = trindicator(element);
 
 				if (i == 0)
 				{
+					// Partition the zero-indicators fairly.
+					i = (zerosThusfar < negativeZeros) ? -1 : 1;
+
 					// The element has zero indicator.
-					++zeros;
-					
-					// Alternate zero-indicator.
-					i = zeroIndicator;
-					zeroIndicator = -zeroIndicator;
+					++zerosThusfar;
 				}
 
 				if (i < 0)
 				{
 					// This element is negative. Move it to
 					// the end of the negative set.
-					*negativeEnd = std::move(*current);
+					*negativeEnd = std::move(element);
 
 					// Update the end of the negative set.
 					++negativeEnd;
@@ -75,52 +98,14 @@ namespace Pastel
 				{
 					// This element is positive. Move it to 
 					// the temporary positive set.
-					positiveSet.emplace_back(std::move(*current));
+					positiveSet.emplace_back(std::move(element));
 				}
-
-				// Proceed to the next element.
-				++current;
 			}
 
 			// Move the temporary positive set after the
 			// negative set.
 			std::move(positiveSet.begin(), positiveSet.end(),
 				negativeEnd);
-
-			if (n >= 2 && zeros > 0)
-			{
-				// There is the possibility that all points
-				// are assigned to the same set, although
-				// there is at least one boundary point.
-				//
-				// In this case we case we want the set B 
-				// (positive/negative) to get the boundary point. 
-				// For example, with kd-trees it is important 
-				// that splitting a node always decreases the 
-				// number of points in a node.
-
-				if (negativeEnd == end)
-				{
-					// All points are in negative set.
-					
-					// Since we have at least one
-					// boundary point, there is exactly
-					// one boundary point, since we
-					// alternate the assignment in the
-					// loop above.
-					ASSERT_OP(zeros, ==, 1);
-
-					// Move that boundary point into
-					// the positive set.
-					--negativeEnd;
-				}
-				else if (negativeEnd == begin)
-				{
-					// All points are in positive set.
-					ASSERT_OP(zeros, ==, 1);
-					++negativeEnd;
-				}
-			}
 
 			// Return the end of the negative set.
 			return negativeEnd;
