63a3ff11feedcf4446425e050ccf6f37b925cc23
kaba2 <kaba2@localhost>
2017-12-04 03:27:56 +0200

    Removed dimensions from distances.

--- pastel/math/distance/distance_base.h
+++ pastel/math/distance/distance_base.h
@@ -3,51 +3,30 @@
 #ifndef PASTELMATH_DISTANCE_BASE_H
 #define PASTELMATH_DISTANCE_BASE_H
 
-#include "pastel/sys/mytypes.h"
-#include "pastel/sys/ensure.h"
 #include "pastel/math/distance/distance_concept.h"
 
 #include <boost/operators.hpp>
-
-#include <cmath>
 
 namespace Pastel
 {
 
 	template <typename Derived, typename Real>
-	struct DistanceBaseOp
+	struct DistanceBase
 	: boost::totally_ordered<Derived
 	, boost::multipliable<Derived, Real
 	, boost::dividable<Derived, Real
 	> > >
-	{};
-
-	template <typename Derived, typename Real, integer N = Dynamic>
-	struct DistanceBase
-	: DistanceBaseOp<Derived, Real>
 	{
-	public:
-		explicit DistanceBase(integer n = D) {
-			PENSURE_OP(n, ==, N);
-		}
-
-		constexpr integer n() const {
-			return N;
-		}
-	};
-
-	template <typename Derived, typename Real>
-	struct DistanceBase<Derived, Real, Dynamic>
-	: DistanceBaseOp<Derived, Real>
-	{
-		explicit DistanceBase(integer n)
-		: n_(n) 
+		bool operator<(const Derived& that) const
 		{
-			//PENSURE_OP(n, >, 0);
+			Derived& self = (Derived&)*this;
+			return self.internal() < that.internal();
 		}
 
-		integer n() const {
-			return n_;
+		bool operator==(const Derived& that) const
+		{
+			Derived& self = (Derived&)*this;
+			return self.internal() == that.internal();
 		}
 
 		Derived& set(integer axis, const Real& amount)
@@ -56,19 +35,6 @@ namespace Pastel
 			self.replace(axis, 0, amount);
 			return self;
 		}
-
-		bool operator<(const Derived& right) const {
-			const Derived& self = (Derived&)*this;
-			return self.internal() < right.internal();
-		}
-
-		bool operator==(const Derived& right) const {
-			const Derived& self = (Derived&)*this;
-			return self.internal() == right.internal();
-		}
-
-	private:
-		integer n_;
 	};
 
 }
