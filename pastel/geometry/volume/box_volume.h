bbcead72c16c79181675cc50e6b048312fed3c25
kaba <kaba@hilvi.org>
2020-07-10 15:40:38 +0300

    Removed .hpp files.

--- pastel/geometry/volume/box_volume.h
+++ pastel/geometry/volume/box_volume.h
@@ -6,21 +6,29 @@
 
 #include "pastel/geometry/shape/alignedbox.h"
 #include "pastel/geometry/shape/box.h"
+#include "pastel/sys/vector/vector.h"
 
 namespace Pastel
 {
 
 	template <typename Real, int N>
-	Real volume(const AlignedBox<Real, N>& box);
+	Real volume(const AlignedBox<Real, N>& box)
+	{
+		return boxVolume(box.max() - box.min());
+	}
 
 	template <typename Real, int N>
-	Real volume(const Box<Real, N>& box);
+	Real volume(const Box<Real, N>& box)
+	{
+		return boxVolume(2 * box.width());
+	}
 
 	template <typename Real, int N>
-	Real boxVolume(const Vector<Real, N>& width);
+	Real boxVolume(const Vector<Real, N>& width)
+	{
+		return product(width);
+	}
 
 }
-
-#include "pastel/geometry/volume/box_volume.hpp"
 
 #endif
--- pastel/geometry/volume/sphere_volume.h
+++ pastel/geometry/volume/sphere_volume.h
@@ -6,9 +6,13 @@
 #define PASTELGEOMETRY_SPHERE_VOLUME_H
 
 #include "pastel/geometry/shape/sphere.h"
+#include "pastel/geometry/area/sphere_area.h"
 
 #include "pastel/sys/math/gamma.h"
 #include "pastel/sys/math/constants.h"
+#include "pastel/sys/math/factorial.h"
+#include "pastel/sys/math/gamma.h"
+#include "pastel/sys/math/powers.h"
 
 namespace Pastel
 {
@@ -19,7 +23,46 @@ namespace Pastel
 	n > 0
 	*/
 	template <typename Real>
-	Real lnVolumeUnitSphereManhattan(integer n);
+	Real lnVolumeUnitSphereManhattan(integer n)
+	{
+		PENSURE_OP(n, >, 0);
+
+		// See volumeUnitSphereManhattan() for the derivation 
+		// of the volume.
+
+		// ln(volume(S))
+		// = ln(2^n / n!)
+		// = n ln(2) - ln(n!)
+
+		return n * constantLn2<Real>() - lnFactorial<Real>(n);
+	}
+
+	template <typename Real>
+	Real volumeUnitSphereManhattan(integer n)
+	{
+		ENSURE_OP(n, >, 0);
+
+		// The volume of Manhattan unit sphere is
+		// 2^n / n!.
+
+		switch(n)
+		{
+		case 1:
+			// 2^1 / 1! = 2
+			return 2;
+			break;
+		case 2:
+			// 2^2 / 2! = 2
+			return 2;
+			break;
+		case 3:
+			// 2^3 / 3! = 8 / 6 = 4 / 3
+			return (Real)4 / 3;
+			break;
+		};
+
+		return std::exp(lnVolumeUnitSphereManhattan<Real>(n));
+	} 
 
 	//! Returns the natural logarithm of the volume of Euclidean unit sphere.
 	/*!
@@ -27,7 +70,20 @@ namespace Pastel
 	n > 0
 	*/
 	template <typename Real>
-	Real lnVolumeUnitSphere(integer n);
+	Real lnVolumeUnitSphere(integer n)
+	{
+		ENSURE_OP(n, >, 0);
+
+		// See volumeUnitSphere() for the derivation 
+		// of the volume.
+
+		// ln(volume(S))
+		// = ln(pi^(n / 2) / gamma(n / 2 + 1))
+		// = (n / 2) ln(pi) - ln(gamma(n / 2 + 1))
+
+		Real nHalf = (Real)n / 2;
+		return nHalf * std::log(constantPi<Real>()) - lnGamma<Real>(nHalf + 1);
+	}
 
 	//! Returns the volume of the Euclidean unit sphere.
 	/*!
@@ -35,7 +91,43 @@ namespace Pastel
 	n > 0
 	*/
 	template <typename Real>
-	Real volumeUnitSphere(integer n);
+	Real volumeUnitSphere(integer n)
+	{
+		ENSURE_OP(n, >, 0);
+
+		// The volume of the Euclidean unit sphere is
+		// pi^(n / 2) / gamma(n / 2 + 1).
+
+		switch(n)
+		{
+		case 1:
+			// pi^(0.5) / gamma(1.5) 
+			// = pi^(0.5) / (0.5 gamma(0.5))
+			// = pi^(0.5) / (0.5 pi^(0.5))
+			// = 1 / 0.5 
+			// = 2
+			return 2;
+			break;
+		case 2:
+			// pi / gamma(2)
+			// = pi / 1!
+			// = pi
+			return constantPi<Real>();
+			break;
+		case 3:
+			// pi^(1.5) / gamma(2.5)
+			// = pi^(1.5) / (1.5 gamma(1.5))
+			// = pi^(1.5) / (1.5 * 0.5 * gamma(0.5))
+			// = pi^(1.5) / (1.5 * 0.5 * pi^(0.5))
+			// = pi / ((3 / 2) * (1 / 2))
+			// = pi / (3 / 4)
+			// = (4 / 3) pi
+			return ((Real)4 / 3) * constantPi<Real>();
+			break;
+		};
+
+		return std::exp(lnVolumeUnitSphere<Real>(n));
+	} 
 
 	//! Returns the natural logarithm of the volume of Minkowski unit sphere.
 	/*!
@@ -46,7 +138,19 @@ namespace Pastel
 	template <typename Real>
 	Real lnVolumeUnitSphereMinkowski(
 		integer n, 
-		const NoDeduction<Real>& power);
+		const NoDeduction<Real>& power)
+	{
+		PENSURE_OP(n, >, 0);
+		PENSURE_OP(power, >, 0);
+
+		Real inversePower = inverse(power);
+
+		// From Wikipedia "Volume of an n-ball"
+		return 
+			lnGamma<Real>(inversePower + 1) - 
+			lnGamma<Real>(n * inversePower + 1) +
+			n * constantLn2<Real>();
+	}
 
 	//! Returns the natural logarithm of the volume of Infinity unit sphere.
 	/*!
@@ -54,10 +158,40 @@ namespace Pastel
 	n >= 0
 	*/
 	template <typename Real>
-	Real lnVolumeUnitSphereInfinity(integer n);
+	Real lnVolumeUnitSphereInfinity(integer n)
+	{
+		PENSURE_OP(n, >, 0);
+
+		// See volumeUnitSphereInfinity() for the
+		// derivation of the volume.
+
+		// ln(volume(S)) 
+		// = ln(2^n)
+		// = n ln(2)
+
+		return n * constantLn2<Real>();
+	}
+
+	template <typename Real>
+	Real volumeUnitSphereInfinity(integer n)
+	{
+		PENSURE_OP(n, >, 0);
+
+		// S = {x in R^n : max(|x_1|, ..., |x_n|) <= 1}
+		//   = [-1, 1]^n
+		// => 
+		// volume(S) = 2^n
+
+		return std::pow((Real)2, (Real)n);
+	}
+
+	template <typename Real, int N>
+	Real volume(const Sphere<Real, N>& sphere)
+	{
+		return volumeUnitSphere<Real>(sphere.n()) * 
+			std::pow(sphere.radius(), sphere.n());
+	}
 
 }
-
-#include "pastel/geometry/volume/sphere_volume.hpp"
 
 #endif
