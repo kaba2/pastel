8db72c8b34c8f131f9f7636d7d2b3f66e03605c1
kaba <kaba@hilvi.org>
2021-01-23 07:38:41 +0200

    Updated build script.

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion 
 
 set buildDir=build
 if not exist %buildDir% (
@@ -8,15 +9,22 @@ if not exist %buildDir% (
 
 set buildType=RelWithDebInfo
 if NOT [%1]==[] (
+    if not "%1"=="Debug" if not "%1"=="Release" if not "%1"=="RelWithDebInfo" (
+        echo Unknown build type %1.
+        echo Build type must be one of Debug, Release, or RelWithDebInfo.
+        goto NARP
+    )
     set buildType=%1
 )
 
-if exist "%buildDir%\CMakeCache.txt" (
+echo Type: !buildType!
+echo Directory: !buildDir!
+
+if exist "!buildDir!\CMakeCache.txt" (
     echo Removing existing CMakeCache.txt...
-    del "%buildDir%\CMakeCache.txt"
+    del "!buildDir!\CMakeCache.txt"
 )
 
-echo Type: %buildType%
-echo Directory: %buildDir%
+cmake -S . -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=!buildType! -B !buildDir!
 
-cmake -S . -G"Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=%buildType% -B %buildDir%
+:NARP
